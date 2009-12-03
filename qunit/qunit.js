e7ca78834b9944a34ae177005afda01e5b1c841e
jeresig <jeresig@gmail.com>
2009-12-03 12:42:47 -0500

    Went a bit further and made extra-sure that the target was specified correctly.

--- qunit/qunit.js
+++ qunit/qunit.js
@@ -189,7 +189,7 @@ var QUnit = {
 				});
 				
 				addEvent(b, "dblclick", function(e) {
-					var target = e ? e.target : window.event.srcElement;
+					var target = e && e.target ? e.target : window.event.srcElement;
 					if ( target.nodeName.toLowerCase() === "strong" ) {
 						var text = "", node = target.firstChild;
 
