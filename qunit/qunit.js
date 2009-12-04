b12e5cb30dceedc19f504c9686591092b26b54cd
Frances Berriman <frances.berriman@bbc.co.uk>
2009-12-04 23:24:23 +0800

    added styles for toolbar

--- qunit/qunit.js
+++ qunit/qunit.js
@@ -434,7 +434,7 @@ addEvent(window, "load", function() {
 			var li = document.getElementsByTagName("li");
 			for ( var i = 0; i < li.length; i++ ) {
 				if ( li[i].className.indexOf("pass") > -1 ) {
-					li[i].style.display = filter.checked ? "none" : "block";
+					li[i].style.display = filter.checked ? "none" : "";
 				}
 			}
 		});
