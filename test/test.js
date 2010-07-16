898004b86c791fe6e200830b04fa3690d9648183
jzaefferer <joern.zaefferer@gmail.com>
2010-07-16 17:35:30 +0200

    Fixing whitespace

--- test/test.js
+++ test/test.js
@@ -71,7 +71,7 @@ module("async setup test", {
 	  stop();
 		setTimeout(function(){
 	    ok(true);
-		  start();
+			start();
 		}, 500);
 	}
 });
@@ -170,23 +170,23 @@ test("testEnvironment reset for next test",function() {
 module("testEnvironment tests");
 
 function makeurl() {
-  var testEnv = QUnit.current_testEnvironment;
-  var url = testEnv.url || 'http://example.com/search';
-  var q   = testEnv.q   || 'a search test';
-  return url + '?q='+encodeURIComponent(q);
+	var testEnv = QUnit.current_testEnvironment;
+	var url = testEnv.url || 'http://example.com/search';
+	var q   = testEnv.q   || 'a search test';
+	return url + '?q='+encodeURIComponent(q);
 }
 
 test("makeurl working",function() {
 	equal( QUnit.current_testEnvironment, this, 'The current testEnvironment is global');
-  equal( makeurl(), 'http://example.com/search?q=a%20search%20test', 'makeurl returns a default url if nothing specified in the testEnvironment');
+	equal( makeurl(), 'http://example.com/search?q=a%20search%20test', 'makeurl returns a default url if nothing specified in the testEnvironment');
 });
 
-module("testEnvironment with makeurl settings",{
-  url:'http://google.com/',
-  q:'another_search_test'
+module("testEnvironment with makeurl settings", {
+	url:'http://google.com/',
+q:'another_search_test'
 });
-test("makeurl working with settings from testEnvironment",function() {
-  equal( makeurl(), 'http://google.com/?q=another_search_test', 'rather than passing arguments, we use test metadata to form the url');
+test("makeurl working with settings from testEnvironment", function() {
+	equal( makeurl(), 'http://google.com/?q=another_search_test', 'rather than passing arguments, we use test metadata to form the url');
 });
 test("each test can extend the module testEnvironment", {
 	q:'hamstersoup'
