c66030f5ae9cc7d72f180800e16aa6333527f76c
Daniel Baulig <daniel.baulig@gmx.de>
2011-01-29 03:19:53 +0100

    Added pixelEquals (+ tests). pixelEquals compares a specified pixel in a canvas to given rgba values. Tests might need rework.

--- qunit/qunit.js
+++ qunit/qunit.js
@@ -342,6 +342,11 @@ var QUnit = {
 		QUnit.push(expected !== actual, actual, expected, message);
 	},
 
+	pixelEquals: function(canvas, x, y, r, g, b, a, message) {
+		var actual = Array.prototype.slice.apply(canvas.getContext('2d').getImageData(x, y, 1, 1).data), expected = [r, g, b, a];
+		QUnit.push(QUnit.equiv(actual, expected), actual, expected, message);
+	},
+
 	raises: function(block, expected, message) {
 		var actual, ok = false;
 	
--- test/test.js
+++ test/test.js
@@ -312,3 +312,81 @@ module("custom assertions");
 		QUnit.reset = reset;
 	});
 })();
+
+
+test("Canvas pixels", function () {
+	var canvas = document.getElementById('qunit-canvas'), context;
+	try {
+		context = canvas.getContext('2d');
+	} catch(e) {
+		// propably no canvas support, just exit
+		return;
+	}
+	context.fillStyle = 'rgba(0, 0, 0, 0)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 0, 0, 0, 0, 0, 0);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(255, 0, 0, 0)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 0, 0, 0, 0, 0, 0);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(0, 255, 0, 0)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 0, 0, 0, 0, 0, 0);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(0, 0, 255, 0)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 0, 0, 0, 0, 0, 0);
+	context.clearRect(0,0,5,5);
+
+	context.fillStyle = 'rgba(0, 0, 0, 0.5)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 0, 0, 0, 0, 0, 127);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(255, 0, 0, 0.5)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 0, 0, 255, 0, 0, 127);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(0, 255, 0, 0.5)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 0, 0, 0, 255, 0, 127);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(0, 0, 255, 0.5)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 0, 0, 0, 0, 255, 127);
+	context.clearRect(0,0,5,5);
+
+	context.fillStyle = 'rgba(0, 0, 0, 0.5)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 2, 2, 0, 0, 0, 127);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(255, 0, 0, 0.5)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 2, 2, 255, 0, 0, 127);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(0, 255, 0, 0.5)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 2, 2, 0, 255, 0, 127);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(0, 0, 255, 0.5)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 2, 2, 0, 0, 255, 127);
+	context.clearRect(0,0,5,5);
+
+	context.fillStyle = 'rgba(0, 0, 0, 1)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 4, 4, 0, 0, 0, 255);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(255, 0, 0, 1)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 4, 4, 255, 0, 0, 255);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(0, 255, 0, 1)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 4, 4, 0, 255, 0, 255);
+	context.clearRect(0,0,5,5);
+	context.fillStyle = 'rgba(0, 0, 255, 1)';
+	context.fillRect(0, 0, 5, 5);
+	pixelEquals(canvas, 4, 4, 0, 0, 255, 255);
+	context.clearRect(0,0,5,5);
+});
