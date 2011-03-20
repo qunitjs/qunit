8f527b37f6205d8948b112030133ae22ac59dbb1
jzaefferer <joern.zaefferer@gmail.com>
2011-03-20 16:18:58 +0100

    Fix order of noglobals check to produce correct introduced/delete error messages

--- qunit/qunit.js
+++ qunit/qunit.js
@@ -881,13 +881,13 @@ function checkPollution( name ) {
 	var old = config.pollution;
 	saveGlobal();
 	
-	var newGlobals = diff( old, config.pollution );
+	var newGlobals = diff( config.pollution, old );
 	if ( newGlobals.length > 0 ) {
 		ok( false, "Introduced global variable(s): " + newGlobals.join(", ") );
 		config.current.expected++;
 	}
 
-	var deletedGlobals = diff( config.pollution, old );
+	var deletedGlobals = diff( old, config.pollution );
 	if ( deletedGlobals.length > 0 ) {
 		ok( false, "Deleted global variable(s): " + deletedGlobals.join(", ") );
 		config.current.expected++;
