f2b99a6991fad6d5705c0511f331f1fa7ddbb80c
jzaefferer <joern.zaefferer@gmail.com>
2011-03-20 17:14:32 +0100

    Don't modify expected-count when checking pollution. The failing assertion isn't expected, so shouldn't be counted. And if expect wasn't used, the count is misleading.

--- qunit/qunit.js
+++ qunit/qunit.js
@@ -884,13 +884,11 @@ function checkPollution( name ) {
 	var newGlobals = diff( config.pollution, old );
 	if ( newGlobals.length > 0 ) {
 		ok( false, "Introduced global variable(s): " + newGlobals.join(", ") );
-		config.current.expected++;
 	}
 
 	var deletedGlobals = diff( old, config.pollution );
 	if ( deletedGlobals.length > 0 ) {
 		ok( false, "Deleted global variable(s): " + deletedGlobals.join(", ") );
-		config.current.expected++;
 	}
 }
 
