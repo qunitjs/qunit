45672103ff6dee1177e9003eaabd9934bbbc710d
Wesley Walser <wwalser@atlassian.com>
2011-08-01 17:13:22 +1000

    cleanup

--- test/plugins/subsuiteRunner/test.js
+++ test/plugins/subsuiteRunner/test.js
@@ -1,15 +1,16 @@
-module("testSuites tests", (function(){
+module( "testSuites tests", (function(){
 	var asyncTest = QUnit.asyncTest,
 		runSuite = QUnit.runSuite;
+
 	return {
 		setup: function(){
 			//proxy asyncTest and runSuite
-			QUnit.asyncTest = window.asyncTest = function(name, callback){
-				ok(true, "asyncTestCalled for each suite");
+			QUnit.asyncTest = window.asyncTest = function( name, callback ){
+				ok( true, "asyncTestCalled for each suite" );
 				callback(); //don't acutally create tests, just call callback
 			};
 			QUnit.runSuite = window.runSuite = function(){
-				ok(true, "runSuite called for each suite");
+				ok( true, "runSuite called for each suite" );
 			};
 			//ensure that subsuite's done doesn't run
 			this.oldDone = QUnit.done;
@@ -23,22 +24,23 @@ module("testSuites tests", (function(){
 	};
 })());
 
-test("proper number of asyncTest and runSuite calls", function(){
-	expect(6);
-	QUnit.testSuites(["one.html", "two.html", "three.html"]);
+test( "proper number of asyncTest and runSuite calls", function(){
+	expect( 6 );
+	QUnit.testSuites( ["one.html", "two.html", "three.html"] );
 });
 
-test("done callback changed", function(){
-	QUnit.testSuites(["dummy.html"]);
-	notEqual(this.oldDone, QUnit.done, 'done callback should be set');
+test( "done callback changed", function(){
+	QUnit.testSuites( ["dummy.html"] );
+	notEqual( this.oldDone, QUnit.done, "done callback should be set" );
 });
 
-module("testStart tests", (function(){
+module( "testStart tests", (function(){
 	var id = QUnit.id;
 	return {
 		setup: function(){
 			//proxy id
-			var fakeElem = this.fakeElem = document.createElement('div');
+			var fakeElem = this.fakeElem = document.createElement( "div" );
+
 			QUnit.id = function(){
 				return fakeElem;
 			}
@@ -49,21 +51,21 @@ module("testStart tests", (function(){
 	};
 })());
 
-test("running message printed", function(){
+test( "running message printed", function(){
 	var hello = "hello world",
 	expected = "Running " + hello + "...<br>&nbsp;";
-	QUnit.testStart({name: hello});
-	equal(this.fakeElem.innerHTML, expected, "innerHTML was set correctly by testStart");
+	QUnit.testStart( {name: hello} );
+	equal( this.fakeElem.innerHTML, expected, "innerHTML was set correctly by testStart" );
 });
 
-module("testDone tests", (function(){
+module( "testDone tests", (function(){
 	var id = QUnit.id;
 	return {
 		setup: function(){
 			//proxy id
-			var fakeElem = this.fakeElem = document.createElement('div');
-			fakeElem.appendChild(document.createElement('ol'));
-			fakeElem.appendChild(document.createElement('ol'));
+			var fakeElem = this.fakeElem = document.createElement( "div" );
+			fakeElem.appendChild( document.createElement( "ol" ) );
+			fakeElem.appendChild( document.createElement( "ol" ) );
 			QUnit.id = function(){
 				return fakeElem;
 			}
@@ -74,23 +76,24 @@ module("testDone tests", (function(){
 	};
 })());
 
-test("test expansions are hidden", function(){
+test( "test expansions are hidden", function(){
 	QUnit.testDone();
-	equal(this.fakeElem.children[0].style.display, "none", "first ol display is none");
-	equal(this.fakeElem.children[1].style.display, "none", "second ol display is none");
+	equal( this.fakeElem.children[0].style.display, "none", "first ol display is none" );
+	equal( this.fakeElem.children[1].style.display, "none", "second ol display is none" );
 });
 
-test("non-ol elements aren't hidden", function(){
-	this.fakeElem.appendChild(document.createElement('span'));
+test( "non-ol elements aren't hidden", function(){
+	this.fakeElem.appendChild( document.createElement( "span" ) );
 
 	QUnit.testDone();
-	notEqual(this.fakeElem.children[2].style.display, "none", "first ol display is none");
+	notEqual( this.fakeElem.children[2].style.display, "none", "first ol display is none" );
 });
 
-module("runSuite tests", (function(){
+module( "runSuite tests", (function(){
 	var getElementsByTagName = document.getElementsByTagName,
 		createElement = document.createElement,
 		runSuite = QUnit.runSuite;
+
 	return {
 		setup: function(){
 			//proxy getElementsByTagName and createElement
@@ -101,18 +104,18 @@ module("runSuite tests", (function(){
 				fakeElement = {
 					appendChild: function(){appendChildCall.called++},
 					setAttribute: function(){setAttributeCall.args = arguments},
-					addEventListener: function(type, callback){iframeLoad.callback = callback;},
+					addEventListener: function( type, callback ){iframeLoad.callback = callback;},
 					contentWindow: {QUnit: iframeQUnitObject},
 					className: "",
 				};
-			
+
 			document.getElementsByTagName = function(){
 				return [fakeElement];
 			};
 			document.createElement = function(){
 				return fakeElement;
 			}
-			
+
 		},
 		teardown: function(){
 			document.getElementsByTagName = getElementsByTagName;
@@ -123,34 +126,34 @@ module("runSuite tests", (function(){
 	};
 })());
 
-test("runSuite different after first run", function(){
+test( "runSuite different after first run", function(){
 	var before = QUnit.runSuite,
 		after;
 	QUnit.runSuite();
 	after = QUnit.runSuite;
-	notEqual(before, after, "runSuite changed after initial run");
+	notEqual( before, after, "runSuite changed after initial run" );
 });
 
-test("iframe only created once", function(){
+test( "iframe only created once", function(){
 	QUnit.runSuite();
-	equal(this.appendChildCall.called, 1, "append child called once");
+	equal( this.appendChildCall.called, 1, "append child called once" );
 	QUnit.runSuite();
-	equal(this.appendChildCall.called, 1, "append child only ever called once");	
+	equal( this.appendChildCall.called, 1, "append child only ever called once" );
 });
 
-test("iframe's QUnit object is modified when iframe source loads", function(){
+test( "iframe's QUnit object is modified when iframe source loads", function(){
 	var before = this.iframeQUnitObject,
 		after;
 	QUnit.runSuite();
 	this.iframeLoad.callback();
-	notEqual(before, after, "iframe's qunit object is modified upon load");
+	notEqual( before, after, "iframe's qunit object is modified upon load");
 });
 
-test("iframe src set to suite passed", function(){
-	var pages = ['testing.html', 'subsuiteRunner.html'];
-	QUnit.runSuite(pages[0]);
-	equal(this.setAttributeCall.args[0], 'src', "src attribute set");
-	equal(this.setAttributeCall.args[1], pages[0], "src attribute set");
-	QUnit.runSuite(pages[1]);
-	equal(this.setAttributeCall.args[1], pages[1], "src attribute set");
+test( "iframe src set to suite passed", function(){
+	var pages = ["testing.html", "subsuiteRunner.html"];
+	QUnit.runSuite( pages[0] );
+	equal( this.setAttributeCall.args[0], "src", "src attribute set" );
+	equal( this.setAttributeCall.args[1], pages[0], "src attribute set" );
+	QUnit.runSuite( pages[1] );
+	equal( this.setAttributeCall.args[1], pages[1], "src attribute set" );
 });
