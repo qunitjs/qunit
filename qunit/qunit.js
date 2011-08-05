9089be2ac1cb131e6469bdbbf2566a8d60bcef38
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-08-05 12:56:12 +0200

    Whitespace

--- qunit/qunit.js
+++ qunit/qunit.js
@@ -15,7 +15,7 @@ var defined = {
 	sessionStorage: (function() {
 		try {
 			return !!sessionStorage.getItem;
-		} catch(e){
+		} catch(e) {
 			return false;
 		}
   })()
@@ -400,7 +400,7 @@ var QUnit = {
 		// A slight delay, to avoid any current callbacks
 		if ( defined.setTimeout ) {
 			window.setTimeout(function() {
-				if(config.semaphore > 0){
+				if (config.semaphore > 0) {
 					return;
 				}
 				if ( config.timeout ) {
@@ -1051,8 +1051,8 @@ QUnit.equiv = function () {
                 parents.push(a);
                 for (i = 0; i < len; i++) {
                     loop = false;
-                    for(j=0;j<parents.length;j++){
-                        if(parents[j] === a[i]){
+                    for (j=0;j<parents.length;j++) {
+                        if (parents[j] === a[i]) {
                             loop = true;//dont rewalk array
                         }
                     }
@@ -1082,8 +1082,8 @@ QUnit.equiv = function () {
 
                 for (i in a) { // be strict: don't ensures hasOwnProperty and go deep
                     loop = false;
-                    for(j=0;j<parents.length;j++){
-                        if(parents[j] === a[i])
+                    for (j=0;j<parents.length;j++) {
+                        if (parents[j] === a[i])
                             loop = true; //don't go down the same path twice
                     }
                     aProperties.push(i); // collect a's properties
@@ -1364,7 +1364,7 @@ function inArray( elem, array ) {
  * QUnit.diff("the quick brown fox jumped over", "the quick fox jumps over") == "the  quick <del>brown </del> fox <del>jumped </del><ins>jumps </ins> over"
  */
 QUnit.diff = (function() {
-	function diff(o, n){
+	function diff(o, n) {
 		var ns = new Object();
 		var os = new Object();
 
@@ -1433,7 +1433,7 @@ QUnit.diff = (function() {
 		};
 	}
 
-	return function(o, n){
+	return function(o, n) {
 		o = o.replace(/\s+$/, '');
 		n = n.replace(/\s+$/, '');
 		var out = diff(o == "" ? [] : o.split(/\s+/), n == "" ? [] : n.split(/\s+/));
