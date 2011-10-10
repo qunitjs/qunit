b87dcd01104dfd39d57690564a9dcacab066f83c
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-10-10 16:53:24 +0200

    Whitespace/formatting fix, remove unnecessary parens

--- qunit/qunit.js
+++ qunit/qunit.js
@@ -916,15 +916,17 @@ function process(last) {
 	config.depth = config.depth ? config.depth+1 : 1;
 
 	while ( config.queue.length && !config.blocking ) {
-		if ( config.updateRate <= 0 || (((new Date()).getTime() - start) < config.updateRate) ) {
+		if ( config.updateRate <= 0 || ( ( new Date().getTime() - start ) < config.updateRate ) ) {
 			config.queue.shift()();
 		} else {
-			window.setTimeout( function(){process(last)}, 13 );
+			window.setTimeout( function(){
+				process( last );
+			}, 13 );
 			break;
 		}
 	}
 	config.depth--;
-	if (last && !config.blocking && !config.queue.length && config.depth === 0) {
+	if ( last && !config.blocking && !config.queue.length && config.depth === 0 ) {
 		done();
 	}
 }
