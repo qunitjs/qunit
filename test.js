cc842fc7ae3ca847e33653749659cffdd5a9b571
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-10-12 22:13:07 +0200

    Update phantomjs runner to phantomjs 1.3

--- test.js
+++ test.js
@@ -1,63 +1,99 @@
-/* 
- * QUnit Qt+WebKit powered headless test runner using Phantomjs
- * 
+/*
+ * Qt+WebKit powered (mostly) headless test runner using Phantomjs
+ *
  * Phantomjs installation: http://code.google.com/p/phantomjs/wiki/BuildInstructions
- * 
+ *
  * Run with:
  *  phantomjs test.js [url-of-your-qunit-testsuite]
- *  
+ *
  * E.g.
- * 	phantomjs test.js http://localhost/qunit/test
+ *      phantomjs test.js http://localhost/qunit/test
  */
 
-function addLogging(done) {
-	var module;
-	QUnit.moduleStart = function(context) {
-		module = context.name;
-	}
-	var current_test_assertions = [];
-	QUnit.testDone = function(result) {
-		var name = module + ": " + result.name;
-		if (result.failed) {
-			console.log("\u001B[31m✖ " + name);
-			for (var i = 0; i < current_test_assertions.length; i++) {
-				console.log("    " + current_test_assertions[i]);
-			}
-			console.log("\u001B[39m");
-		}
-		current_test_assertions = [];
-	};
-  
-	QUnit.log = function(details) {
-		if (details.result) {
-			return;
-		}
-		var response = details.message || "";
-		if (details.expected) {
-			if (response) {
-				response += ", ";
-			}
-			response = "expected: " + details.expected + ", but was: " + details.actual;
-		}
-		current_test_assertions.push("Failed assertion: " + response);
-	};
-  
-	QUnit.done = function(result) {
-		console.log("Took " + result.runtime + "ms to run " + result.total + " tests. \u001B[32m✔ " + result.passed + "\u001B[39m \u001B[31m✖ " + result.failed + "\u001B[39m ");
-		done(result.failed > 0 ? 1 : 0);
-	};
-}
+var url = phantom.args[0];
 
-if (phantom.state.length === 0) {
-	phantom.state = Date.now().toString();
-	phantom.open(phantom.args[0]);
-} else {
-	if (phantom.loadStatus === 'success') {
-		addLogging(function(returnCode) {
-			phantom.exit(returnCode);
-		});
-	} else {
-		console.log(phantom.loadStatus + ' to load the address: ' + phantom.args[0]);
-		phantom.exit(1);
-	}
+var page = require('webpage').create();
+
+//Route "console.log()" calls from within the Page context to the main Phantom context (i.e. current "this")
+page.onConsoleMessage = function(msg) {
+  console.log(msg);
+};
+
+page.open(url, function(status){
+  if (status !== "success") {
+    console.log("Unable to access network: " + status);
+    phantom.exit(1);
+  } else {
+    page.evaluate(addLogging);
+    var interval = setInterval(function() {
+      if (finished()) {
+        clearInterval(interval);
+        onfinishedTests();
+      }
+    }, 500);
+  }
+});
+
+function finished() {
+  return page.evaluate(function(){
+    return !!window.qunitDone;
+  });
+};
+
+function onfinishedTests() {
+  var output = page.evaluate(function() {
+      return JSON.stringify(window.qunitDone);
+  });
+  phantom.exit(JSON.parse(output).failed > 0 ? 1 : 0);
+};
+
+function addLogging() {
+  var current_test_assertions = [];
+  var module;
+
+  QUnit.moduleStart = function(context) {
+    module = context.name;
+  };
+
+  QUnit.testDone = function(result) {
+    var name = module + ': ' + result.name;
+    var i;
+
+    if (result.failed) {
+      console.log('\u001B[31m✖ ' + name);
+
+      for (i = 0; i < current_test_assertions.length; i++) {
+        console.log('    ' + current_test_assertions[i]);
+      }
+
+      console.log('\u001B[39m');
+    }
+
+    current_test_assertions = [];
+  };
+
+  QUnit.log = function(details) {
+    var response;
+
+    if (details.result) {
+      return;
+    }
+
+    response = details.message || '';
+
+    if (typeof details.expected !== 'undefined') {
+      if (response) {
+        response += ', ';
+      }
+
+      response += 'expected: ' + details.expected + ', but was: ' + details.actual;
+    }
+
+    current_test_assertions.push('Failed assertion: ' + response);
+  };
+
+  QUnit.done = function(result){
+    console.log('Took ' + result.runtime +  'ms to run ' + result.total + ' tests. \u001B[32m✔ ' + result.passed + '\u001B[39m \u001B[31m✖ ' + result.failed + '\u001B[39m ');
+    window.qunitDone = result;
+  };
 }
