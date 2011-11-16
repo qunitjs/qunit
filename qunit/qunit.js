5b487f58c3e2eb9563ffad1c8617ee7dd47207b2
Seong-A Kong <simonz@daumcorp.com>
2011-11-16 14:52:01 +0900

    Show exception stack when test failed

--- qunit/qunit.js
+++ qunit/qunit.js
@@ -995,6 +995,7 @@ function fail(message, exception, callback) {
 	if ( typeof console !== "undefined" && console.error && console.warn ) {
 		console.error(message);
 		console.error(exception);
+		console.error(exception.stack);
 		console.warn(callback.toString());
 
 	} else if ( window.opera && opera.postError ) {
