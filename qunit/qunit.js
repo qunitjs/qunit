6f2156d6ef6dbd3cba2ea49a77aa7c5b46ccf114
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-02-15 17:39:47 +0100

    Improve global object access, based on comments for 1a9120651d5464773256d8a1f2cf2eabe38ea5b3

--- qunit/qunit.js
+++ qunit/qunit.js
@@ -1621,4 +1621,5 @@ QUnit.diff = (function() {
 	};
 })();
 
-})( typeof global == 'object' && global || this );
+// get at whatever the global object is, like window in browsers
+})( (function() {return this}).call() );
