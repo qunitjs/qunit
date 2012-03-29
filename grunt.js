cad5d9fdd51cb323eeadb830de69fbc4a69fe031
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-03-29 12:56:36 +0200

    Update to latest Grunt

--- grunt.js
+++ grunt.js
@@ -1,53 +1,57 @@
 /*global config:true, task:true*/
-config.init({
-  pkg: '<json:package.json>',
-  qunit: {
-    // TODO include 'test/logs.html' as well
-    files: ['test/index.html']
-  },
-  lint: {
-    qunit: 'qunit/qunit.js',
-    grunt: 'grunt.js'
-    // TODO need to figure out which warnings to fix and which to disable
-    // tests: 'test/*.js'
-  },
-  jshint: {
-    qunit: {
-      options: {
-        browser: true,
-        bitwise: true,
-        curly: true,
-        trailing: true,
-        immed: true,
-        latedef: false,
-        newcap: true,
-        noarg: false,
-        noempty: true,
-        nonew: true,
-        sub: true,
-        undef: true,
-        eqnull: true,
-        proto: true
-      },
-      globals: {
-        jQuery: true,
-        exports: true
-      }
-    },
-    tests: {
-      tests: {
-        globals: {
-          module: true,
-          test: true,
-          ok: true,
-          equal: true,
-          deepEqual: true,
-          QUnit: true
-        }
-      }
-    }
-  }
+module.exports = function( grunt ) {
+
+grunt.initConfig({
+	pkg: '<json:package.json>',
+	qunit: {
+		// TODO include 'test/logs.html' as well
+		files: ['test/index.html']
+	},
+	lint: {
+		qunit: 'qunit/qunit.js',
+		grunt: 'grunt.js'
+		// TODO need to figure out which warnings to fix and which to disable
+		// tests: 'test/*.js'
+	},
+	jshint: {
+		qunit: {
+			options: {
+				browser: true,
+				bitwise: true,
+				curly: true,
+				trailing: true,
+				immed: true,
+				latedef: false,
+				newcap: true,
+				noarg: false,
+				noempty: true,
+				nonew: true,
+				sub: true,
+				undef: true,
+				eqnull: true,
+				proto: true
+			},
+			globals: {
+				jQuery: true,
+				exports: true
+			}
+		},
+		tests: {
+			tests: {
+				globals: {
+					module: true,
+					test: true,
+					ok: true,
+					equal: true,
+					deepEqual: true,
+					QUnit: true
+				}
+			}
+		}
+	}
 });
 
 // Default task.
-task.registerTask('default', 'lint qunit');
+grunt.registerTask('default', 'lint qunit');
+
+};
