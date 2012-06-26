1f83eab55edc3392651b5ed40325ea2b889fd17c
Alex J Burke <alex@alexjeffburke.com>
2012-06-26 08:39:26 +0100

    Do not repeat the opening/closing table tags.

--- qunit/qunit.js
+++ qunit/qunit.js
@@ -783,18 +783,22 @@ extend( QUnit, {
 				message: message
 			};
 
-		message = escapeInnerText(message ) || "error";
+		message = escapeInnerText( message ) || "error";
 		message = "<span class='test-message'>" + message + "</span>";
 		output = message;
 
+		output += "<table>";
+
 		if ( actual ) {
-			output += "<table><tr class='test-actual'><th>Result: </th><td><pre>" + escapeInnerText( actual ) + "</pre></td></tr></table>";
+			output += "<tr class='test-actual'><th>Result: </th><td><pre>" + escapeInnerText( actual ) + "</pre></td></tr>";
 		}
 
 		if ( source ) {
 			details.source = source;
-			output += "<table><tr class='test-source'><th>Source: </th><td><pre>" + escapeInnerText( source ) + "</pre></td></tr></table>";
+			output += "<tr class='test-source'><th>Source: </th><td><pre>" + escapeInnerText( source ) + "</pre></td></tr>";
 		}
+
+		output += "</table>";
 
 		runLoggingCallbacks( "log", QUnit, details );
 
