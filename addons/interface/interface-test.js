674789b7d03fc56b2241a567c61a9bd012fe0535
dotnetcarpenter <jon.ronnenberg@gmail.com>
2012-08-08 19:44:45 +0200

    It's hard to see a use-case for the not* methods other than testing the test code it self. But then the methods should share code

--- addons/interface/interface-test.js
+++ addons/interface/interface-test.js
@@ -53,11 +53,11 @@ var testStructure1 = testStructure2 = {
     "class": "elements_1"
 };
 var testStructure3 = {
-    "id": "84",
+    "id": "not a number",
     "language": "en-GB",
     "nappUpdate": "2012-06-08 15:52:02",
     "title": "Intro Plain text",
-    "dataUrl": "http://wexiing.com/index.php?option=com_xing&task=userdata.load&cid=527&mid=1&id=84",
+    "dataUrl": "URI",
     "senddataUrl": "http://wexiing.com/index.php?option=com_xing&task=userdata.save&cid=527&mid=1&id=84",
     "xtype": "pageinfo",
     "elements": [
@@ -78,31 +78,26 @@ var testStructure4 = {
     "senddataUrl": "http://wexiing.com/index.php?option=com_xing&task=userdata.save&cid=527&mid=1&id=84",
     "xtype": "pageinfo",
     "class": "elements_1",
-    "elements": [],
-    "dataUrl": "url"
+    "elements": []
 };
 
-test("structure", 3, function () {
-	QUnit.structure(testStructure2, testStructure1, "these two should have the same structure");
-	QUnit.structure(testStructure3, testStructure1);
-	QUnit.structure(testStructure4, testStructure1);
+test("equalStructure", 2, function () {
+	QUnit.equalStructure(testStructure2, testStructure1, "These two objects have the same structure.");
+	QUnit.equalStructure(testStructure3, testStructure1, "These two objects have the same structure on the first level but not the same data");
+	//TODO: should be in a notEqualStructure testQUnit.equalStructure(testStructure4, testStructure1);
 });
-/*
-test("structureDeep", function () {
-	var halfPi = Math.PI / 2,
-		sqrt2 = Math.sqrt(2);
-
-	QUnit.notClose(6, 7, 0);
-	QUnit.notClose(7, 7.2, 0.1);
-	QUnit.notClose(7, 7.2, 0.19999999999);
-
-	QUnit.notClose(3.141, Math.PI, 0.0001);
-	QUnit.notClose(3.1, Math.PI, 0.001);
-
-	QUnit.notClose(halfPi, 1.57, 0.0001);
-
-	QUnit.notClose(sqrt2, 1.4142, 0.00001);
-
-	QUnit.notClose(Infinity, -Infinity, 5);
+test("deepEqualStructure", 1, function () {
+	QUnit.deepEqualStructure(testStructure2, testStructure1, "These two objects have the same structure.");
+//	QUnit.deepEqualStructure(testStructure3, testStructure1, "These two objects have only the same structure on the first level.");
+//	QUnit.deepEqualStructure(testStructure4, testStructure1);
 });
-*/
+test("notEqualStructure", 1, function () {
+//	QUnit.notEqualStructure(testStructure2, testStructure1, "These two objects have the same structure.");
+//	QUnit.notEqualStructure(testStructure3, testStructure1);
+	QUnit.notEqualStructure(testStructure4, testStructure1);
+});
+test("notDeepEqualStructure", 2, function () {
+//	QUnit.notDeepEqualStructure(testStructure2, testStructure1, "These two objects have the same structure.");
+	QUnit.notDeepEqualStructure(testStructure3, testStructure1, "These two objects have only the same structure on the first level.");
+	QUnit.notDeepEqualStructure(testStructure4, testStructure1);
+});
--- addons/interface/qunit-interface.js
+++ addons/interface/qunit-interface.js
@@ -1,14 +1,57 @@
+/**
+ * It's hard to see a use-case for the not* methods other than testing the test code it self. But then the methods should share code :-S 
+ */
+
 QUnit.extend( QUnit, {
-	structure: function(actual, expected, message) {
+	equalStructure: function(actual, expected, message) {
 		for(var prop in expected) {
 			if(!actual[prop]) {
 				QUnit.push(false, actual, expected, message || prop + ' is not a property of the actual object');
-				return; // if we continue and expected number of tests is set, then continuing will trigger an "Expected assertions run" failure
+				return false; // if we continue and expected number of tests is set, then continuing will trigger an "Expected assertions run" failure
 			}
 		}
 		QUnit.push(true, actual, expected, message);
 	},
-	structureDeep: function(actual, expected, message) {
-		for (var prop)
+	deepEqualStructure: function(actual, expected, message) {
+		this.structureTestingFor += ""; // create variable in QUnit
+		for (var prop in expected) {
+			//testProperty instanceof Object && typeof testProperty != 'function'
+			if(typeof actual[prop] === 'object') {
+				this.structureTestingFor = prop;
+				if(!QUnit.deepEqualStructure(actual[prop], expected[prop], message)) {
+					return false; // fail
+				}
+			} else {
+				if(!actual[prop]) {
+					QUnit.push(false, actual, expected, message || 'Expected ' + (this.structureTestingFor || prop) + ' but found ' + prop + '.');
+					return false; // fail
+				}
+				//continue;
+			}
+		}
+		delete this.structureTestingFor; // clean up variable in QUnit
+		QUnit.push(true, actual, expected, message);
+	},
+	notEqualStructure: function(actual, expected, message) {
+		for(var prop in expected) {
+			if(!actual[prop]) {
+				QUnit.push(true, actual, expected, message || prop + ' is not a property of the actual object');
+				return;	// if we continue and expected number of tests is set, then continuing will trigger an "Expected assertions run" failure
+			}
+		}
+		QUnit.push(false, actual, expected, message || 'The structures are equal at the first level.');
+	},
+	notDeepEqualStructure: function(actual, expected, message) {
+		for (var prop in expected) {
+			if(typeof actual[prop] === 'object') {
+				return QUnit.notDeepEqualStructure(actual[prop], expected[prop], message)
+			} else {
+				if(!actual[prop]) {
+					QUnit.push(true, actual, expected, message || prop + ' is not a property of the actual object.');
+					return true;	// yes they doesn't have equal structure
+				}
+			}
+		}
+		QUnit.push(false, actual, expected, message || 'The structures are equal.');
 	}
-});
+})
