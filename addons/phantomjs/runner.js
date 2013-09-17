b2340614dd4bfbbbdced44891de137ed17fca67b
Vernon Kesner <vernonkesner@gmail.com>
2013-09-17 16:54:52 -0400

    Updating PhantomJS runner to include colored output

--- addons/phantomjs/runner.js
+++ addons/phantomjs/runner.js
@@ -11,33 +11,35 @@
  *   phantomjs runner.js http://localhost/qunit/test/index.html
  */
 
+/*jshint latedef:false */
 /*global phantom:false, require:false, console:false, window:false, QUnit:false */
 
 (function() {
 	'use strict';
 
-	var url, page, timeout,
-		args = require('system').args;
+	var args = require( "system" ).args;
 
 	// arg[0]: scriptName, args[1...]: arguments
-	if (args.length < 2 || args.length > 3) {
-		console.error('Usage:\n  phantomjs runner.js [url-of-your-qunit-testsuite] [timeout-in-seconds]');
+	if (args.length !== 2) {
+		console.error('Usage:\n  phantomjs runner.js [url-of-your-qunit-testsuite]');
 		phantom.exit(1);
 	}
 
-	url = args[1];
-	page = require('webpage').create();
-	if (args[2] !== undefined) {
-		timeout = parseInt(args[2], 10);
-	}
+	var url = args[1],
+		page = require('webpage').create();
 
 	// Route `console.log()` calls from within the Page context to the main Phantom context (i.e. current `this`)
 	page.onConsoleMessage = function(msg) {
+		if ( /MOCK\ GET/.test( msg ) ) {
+			return;
+		}
+
 		console.log(msg);
 	};
 
 	page.onInitialized = function() {
 		page.evaluate(addLogging);
+		page.evaluate( colorizer );
 	};
 
 	page.onCallback = function(message) {
@@ -67,21 +69,48 @@
 				phantom.exit(1);
 			}
 
-			// Set a timeout on the test running, otherwise tests with async problems will hang forever
-			if (typeof timeout === 'number') {
-				setTimeout(function() {
-					console.error('The specified timeout of ' + timeout + ' seconds has expired. Aborting...');
-					phantom.exit(1);
-				}, timeout * 1000);
-			}
-
 			// Do nothing... the callback mechanism will handle everything!
 		}
 	});
 
+	function colorizer() {
+		window.ANSI = {};
+		window.ANSI.color_map = {
+			"red": "\u001b[31m",
+			"redBold": "\u001b[1m\u001b[31m",
+			"green": "\u001b[32m",
+			"greenBold": "\u001b[1m\u001b[32m",
+			"blue": "\u001b[34m",
+			"blueBold": "\u001b[1m\u001b[34m",
+			"end": "\u001b[0m"
+		};
+
+		window.ANSI.highlight_map = {
+			"red": "\u001b[41m \u001b[37m", // change 37 to 30 for black text
+			"green": "\u001b[42m \u001b[30m",
+			"blue": "\u001b[44m \u001b[37m",
+			"end": "\u001b[0m"
+		};
+
+		window.ANSI.highlight_text = function ( text, color ) {
+			var color_code = this.highlight_map[ color ],
+					color_end = this.highlight_map.end;
+
+			return color_code + text + color_end;
+		};
+
+		window.ANSI.colorize_text = function ( text, color ) {
+			var color_code = this.color_map[ color ],
+					color_end = this.color_map.end;
+
+			return color_code + text + color_end;
+
+		};
+	}
+
 	function addLogging() {
 		window.document.addEventListener('DOMContentLoaded', function() {
-			var currentTestAssertions = [];
+			var current_test_assertions = [];
 
 			QUnit.log(function(details) {
 				var response;
@@ -99,13 +128,18 @@
 					}
 
 					response += 'expected: ' + details.expected + ', but was: ' + details.actual;
+					if (details.source) {
+						response += "\n" + details.source;
+					}
 				}
 
-				if (details.source) {
-					response += "\n" + details.source;
-				}
+				current_test_assertions.push( window.ANSI.colorize_text( 'Failed assertion: ', "red" ) + response);
+			});
 
-				currentTestAssertions.push('Failed assertion: ' + response);
+			QUnit.moduleStart( function ( details ) {
+				console.log( "---------------" );
+				console.log( "Running QUnit Tests for: " + window.ANSI.colorize_text( details.name, "blueBold") );
+				console.log( "---------------" );
 			});
 
 			QUnit.testDone(function(result) {
@@ -114,18 +148,23 @@
 					name = result.module + ': ' + result.name;
 
 				if (result.failed) {
-					console.log('Test failed: ' + name);
 
-					for (i = 0, len = currentTestAssertions.length; i < len; i++) {
-						console.log('    ' + currentTestAssertions[i]);
+					console.log( window.ANSI.highlight_text( String.fromCharCode( "0x2717" ) + " Test failed: ", "red" ) + " " + name );
+
+					for (i = 0, len = current_test_assertions.length; i < len; i++) {
+						console.log('    ' + String.fromCharCode( "0x21B3" ) + "  " + current_test_assertions[i]);
 					}
+				} else {
+					console.log( window.ANSI.highlight_text( String.fromCharCode( "0x2713" ) + " Test passed: ", "green" ) + " " + name );
 				}
 
-				currentTestAssertions.length = 0;
+				current_test_assertions.length = 0;
 			});
 
 			QUnit.done(function(result) {
-				console.log('Took ' + result.runtime +  'ms to run ' + result.total + ' tests. ' + result.passed + ' passed, ' + result.failed + ' failed.');
+				console.log( "---------------" );
+				console.log('Took ' + result.runtime +  'ms to run ' + result.total + ' tests. ' + window.ANSI.colorize_text( result.passed + ' passed', "green" ) + ', ' + window.ANSI.colorize_text( result.failed + ' failed', "red" ) + '.');
+				console.log( "---------------" );
 
 				if (typeof window.callPhantom === 'function') {
 					window.callPhantom({
