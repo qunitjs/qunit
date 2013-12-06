da6d7cd576b3491c64ccb53486de9e7b72e8c2c9
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-12-06 16:07:55 +0100

    Core: Silence addEvent in non-browser env

--- src/core.js
+++ src/core.js
@@ -1036,7 +1036,7 @@ function addEvent( elem, type, fn ) {
 	if ( elem.addEventListener ) {
 		elem.addEventListener( type, fn, false );
 	// IE
-	} else {
+	} else if ( elem.attachEvent ) {
 		elem.attachEvent( "on" + type, fn );
 	}
 }
