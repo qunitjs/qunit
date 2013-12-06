66535f4f9d36a46072f970bf74a1e9ada0980fe8
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-12-06 16:53:34 +0100

    Test/node/narwhal: Update require path, execute QUnit.load()
    The require path changed during the module refactor, but wasn't updated here.
    The QUnit.load() call is currently necessary for any tests to run. This needs
    another review.

--- test/narwhal-test.js
+++ test/narwhal-test.js
@@ -1,6 +1,7 @@
 /*jshint node:true, undef:false */
+/*globals QUnit:true */
 // Run with: $ narwhal test/narwhal-test.js
-var QUnit = require("../qunit/qunit");
+var QUnit = require("../dist/qunit");
 
 QUnit.log(function(details) {
 	if (!details.result) {
@@ -19,5 +20,8 @@ QUnit.test("fail twice with stacktrace", function(assert) {
 	/*jshint expr:true */
 	assert.equal(true, false);
 	assert.equal(true, false, "gotta fail");
-	x.y.z; // Throws ReferenceError
+	// Throws ReferenceError
+	x.y.z;
 });
+
+QUnit.load();
