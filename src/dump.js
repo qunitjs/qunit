407561672d26b2a2405c1505f4b5b8e2837f2f92
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-05-08 21:04:49 +0200

    Revert "jsDump: Output non-enumerable properties of TypeError"
    This reverts commit faa3809077d090c1ee7371dbc574b46209bd7895.
    
    Too many failures in browsers other then Chrome, missed before initial push.

--- src/dump.js
+++ src/dump.js
@@ -151,22 +151,11 @@ QUnit.jsDump = (function() {
 				"arguments": array,
 				object: function( map, stack ) {
 					/*jshint forin:false */
-					var ret = [], keys, key, val, i, nonEnumerableProperties;
+					var ret = [], keys, key, val, i;
 					QUnit.jsDump.up();
 					keys = [];
 					for ( key in map ) {
 						keys.push( key );
-					}
-
-					// No enumerable properties, look for specific non-enumerable properties.
-					if ( keys.length === 0 ) {
-						nonEnumerableProperties = [ "message", "name" ];
-						for ( i in nonEnumerableProperties ) {
-							key = nonEnumerableProperties[ i ];
-							if ( key in map ) {
-								keys.push( key );
-							}
-						}
 					}
 					keys.sort();
 					for ( i = 0; i < keys.length; i++ ) {
