9df2550c844bf52c5d612f42c8e1f8661aaf79e9
Anne-Gaelle Colom <coloma@westminster.ac.uk>
2014-12-27 11:07:07 +0000

    Build: Remove dates from copyright notice

--- src/intro.js
+++ src/intro.js
@@ -2,7 +2,7 @@
  * QUnit @VERSION
  * http://qunitjs.com/
  *
- * Copyright 2006, 2014 jQuery Foundation and other contributors
+ * Copyright jQuery Foundation and other contributors
  * Released under the MIT license
  * http://jquery.org/license
  *
