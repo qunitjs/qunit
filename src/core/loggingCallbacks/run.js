a8edb03bf6c4174a84a7a4e77be9f4882f676dd5
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-03-17 20:55:01 +0100

    Core: put all loggingCallback logic inside their own directory

---
+++ src/core/loggingCallbacks/run.js
@@ -0,0 +1,8 @@
+function runLoggingCallbacks( key, args ) {
+	var i, l, callbacks;
+
+	callbacks = config.callbacks[ key ];
+	for ( i = 0, l = callbacks.length; i < l; i++ ) {
+		callbacks[ i ]( args );
+	}
+}
