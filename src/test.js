47314e1ef06df966d72d76b2f90204b29840f0a2
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-03-29 17:53:31 +0200

    Core: core/test.js moved to test.js

--- src/test.js
+++ src/test.js
@@ -487,3 +487,42 @@ function checkPollution() {
 		QUnit.pushFailure( "Deleted global variable(s): " + deletedGlobals.join( ", " ) );
 	}
 }
+
+// Will be exposed as QUnit.asyncTest
+function asyncTest( testName, expected, callback ) {
+	if ( arguments.length === 2 ) {
+		callback = expected;
+		expected = null;
+	}
+
+	QUnit.test( testName, expected, callback, true );
+}
+
+// Will be exposed as QUnit.test
+function test( testName, expected, callback, async ) {
+	var newTest;
+
+	if ( arguments.length === 2 ) {
+		callback = expected;
+		expected = null;
+	}
+
+	newTest = new Test({
+		testName: testName,
+		expected: expected,
+		async: async,
+		callback: callback
+	});
+
+	newTest.queue();
+}
+
+// Will be exposed as QUnit.skip
+function skip( testName ) {
+	var test = new Test({
+		testName: testName,
+		skip: true
+	});
+
+	test.queue();
+}
