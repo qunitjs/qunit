2c14c6bb5f1383802628f9e0c8c3f2420e5c7222
Leonardo Balter <leonardo.balter@gmail.com>
2015-04-16 00:46:25 -0300

    Tests: Avoid loosen errors on autostart test
    Also remove the QUnit.load as they were invalid
    
    Fixes #798
    Closes #803

--- test/autostart.js
+++ test/autostart.js
@@ -1,13 +1,11 @@
-/*global asyncDelay, times, beginData */
+/*global times, beginData */
 
 QUnit.start();
 
 QUnit.module( "autostart" );
 
 QUnit.test( "Prove the test run started as expected", function( assert ) {
-	assert.expect( 4 );
-	assert.ok( times.autostartOff <= times.pageLoaded );
-	assert.ok( times.pageLoaded <= times.runStarted );
-	assert.ok( times.autostartOff + asyncDelay <= times.runStarted );
+	assert.expect( 2 );
+	assert.ok( times.autostartOff <= times.runStarted );
 	assert.strictEqual( beginData.totalTests, 1, "Should have expected 1 test" );
 });
