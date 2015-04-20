bdb319f5763efcfc9fd746e227af28d1d3858f1e
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-04-20 15:16:25 +0200

    Test: fixed style issues

--- src/test.js
+++ src/test.js
@@ -460,6 +460,7 @@ function saveGlobal() {
 	if ( config.noglobals ) {
 		for ( var key in window ) {
 			if ( hasOwn.call( window, key ) ) {
+
 				// in Opera sometimes DOM element ids show up here, ignore them
 				if ( /^qunit-test-output/.test( key ) ) {
 					continue;
