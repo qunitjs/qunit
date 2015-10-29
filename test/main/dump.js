4a8889d59d9266f94511882185563b19d6ed4057
paladox <paladox@users.noreply.github.com>
2015-10-29 13:18:20 +0000

    Update dump.js

--- test/main/dump.js
+++ test/main/dump.js
@@ -17,7 +17,7 @@ QUnit.test( "dump output", function( assert ) {
 			"[\n  <h1 id=\"qunit-header\"></h1>\n]"
 		);
 	}
-});
+} );
 
 QUnit.test( "dump output, shallow", function( assert ) {
 	var obj = {
@@ -49,7 +49,7 @@ QUnit.test( "dump output, shallow", function( assert ) {
 		QUnit.dump.parse( obj ),
 		"{\n  \"left\": 0,\n  \"top\": {\n    \"middle\": {\n      \"bottom\": 0\n    }\n  }\n}"
 	);
-});
+} );
 
 QUnit.test( "dump, TypeError properties", function( assert ) {
 	function CustomError( message ) {
@@ -95,7 +95,7 @@ QUnit.test( "dump, TypeError properties", function( assert ) {
 			dumpedTypeErrorWithEnumerable,
 			expectedTypeMessage,
 			"type error with enumerable field contains message field" );
-});
+} );
 
 QUnit.module( "dump, recursions", {
 	Wrap: function( x ) {
@@ -118,7 +118,7 @@ QUnit.module( "dump, recursions", {
 
 		return last;
 	}
-});
+} );
 
 QUnit.test( "Check dump recursion", function( assert ) {
 	assert.expect( 4 );
@@ -144,7 +144,7 @@ QUnit.test( "Check dump recursion", function( assert ) {
 	assert.ok( new RegExp( "recursion\\(-10\\)" ).test( circdump ),
 		"(" + circdump + ") should show -10 recursion level"
 	);
-});
+} );
 
 QUnit.test( "Check equal/deepEqual recursion", function( assert ) {
 	var noRecursion, selfref, circref;
@@ -160,7 +160,7 @@ QUnit.test( "Check equal/deepEqual recursion", function( assert ) {
 	circref = this.chainwrap( 10 );
 	assert.equal( circref, circref, "Or hide that through some levels of indirection." );
 	assert.deepEqual( circref, circref, "... and checked on all levels!" );
-});
+} );
 
 QUnit.test( "Circular reference with arrays", function( assert ) {
 	var arr, arrdump, obj, childarr, objdump, childarrdump;
@@ -192,7 +192,7 @@ QUnit.test( "Circular reference with arrays", function( assert ) {
 		"no endless stack when trying to dump array/object mix with circular ref"
 	);
 
-});
+} );
 
 QUnit.test( "Circular reference - test reported by soniciq in #105", function( assert ) {
 	var a, b, barr,
@@ -219,4 +219,4 @@ QUnit.test( "Circular reference - test reported by soniciq in #105", function( a
 
 	assert.equal( a.children(), barr );
 	assert.deepEqual( a.children(), [ b ] );
-});
+} );
