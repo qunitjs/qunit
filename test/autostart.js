6953b0e577eddb98b963b38bd50cee74fccc0433
paladox <paladox@users.noreply.github.com>
2015-10-29 11:39:38 +0000

    Update autostart.js

--- test/autostart.js
+++ test/autostart.js
@@ -8,4 +8,4 @@ QUnit.test( "Prove the test run started as expected", function( assert ) {
 	assert.expect( 2 );
 	assert.ok( times.autostartOff <= times.runStarted );
 	assert.strictEqual( beginData.totalTests, 1, "Should have expected 1 test" );
-});
+} );
