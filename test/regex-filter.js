898bac046ccd0105171a1d893be860d390a7572e
Trent Willis <twillis@linkedin.com>
2015-12-15 16:47:06 -0800

    Core: Support filtering by regular expression
    Introduces the ability to filter by regular expression by passing in a string
    formatted as a regex literal.

---
+++ test/regex-filter.js
@@ -0,0 +1,17 @@
+QUnit.module( "QUnit.config.filter with case-insensitive regular expression" );
+
+QUnit.test( "foo test should be run", function( assert ) {
+	assert.ok( true, "foo test should be run" );
+});
+
+QUnit.test( "boo test should not be run", function( assert ) {
+	assert.ok( false, "boo test should not be run" );
+});
+
+QUnit.test( "bar test should be run", function( assert ) {
+	assert.ok( true, "bar test should be run" );
+});
+
+QUnit.test( "baz test should not be run", function( assert ) {
+	assert.ok( false, "baz test should not be run" );
+});
