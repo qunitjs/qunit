093a344ac77618642cb2df6bee1d64041214a4f0
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2015-12-30 15:10:04 +0900

    Test: Apply review

--- test/reorderError1.js
+++ test/reorderError1.js
@@ -1,10 +1,7 @@
-/*global totalCount */
-
 QUnit.module( "Test call count" );
-// Then
 QUnit.test( "should be success", function( assert ) {
 	if ( window.sessionStorage ) {
-	    assert.equal( totalCount, 3 );
+		assert.equal( window.totalCount, 3 );
 	} else {
 		assert.ok(true);
 	}
