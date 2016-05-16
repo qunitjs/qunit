b04b03b0eee0c15ed4cfbb0a501f732760e4a203
Cory Forsyth <cory.forsyth@gmail.com>
2016-05-16 15:51:46 -0700

    Core: Decode "+" to " " (space) in url params
    This fixes the decoding part of issue gh-1000.
    
    Ref gh-1000
    Fixes gh-1000
    Closes gh-1001

--- test/reporter-urlparams.js
+++ test/reporter-urlparams.js
@@ -1,23 +1,26 @@
 if ( !location.search ) {
 	location.replace( "?implicit&explicit=yes&array=A&array=B&escaped%20name&" +
-		"module=urlParams&notrycatch&custom&customArray=a&customArray=b" );
+		"module=urlParams+module&notrycatch&custom&customArray=a&customArray=b&" +
+		"filter=urlParams%20module" );
 }
 
 QUnit.config.urlConfig.push( "custom", "customArray" );
 
 // Don't change this module name without also changing the module parameter when loading this suite
-QUnit.module( "urlParams", function() {
+QUnit.module( "urlParams module", function() {
 	QUnit.test( "parsing", function( assert ) {
 		assert.ok( QUnit.urlParams, "urlParams property exists" );
 		assert.strictEqual( QUnit.urlParams.implicit, true, "implicit true value" );
 		assert.strictEqual( QUnit.urlParams.explicit, "yes", "explicit value" );
 		assert.deepEqual( QUnit.urlParams.array, [ "A", "B" ], "multiple values" );
 		assert.ok( QUnit.urlParams[ "escaped name" ], "escape sequences in name" );
+		assert.strictEqual( QUnit.urlParams.filter, "urlParams module", "escaped space as %20" );
 	} );
 
 	QUnit.module( "QUnit.config properties", function() {
 		QUnit.test( "setting standard properties", function( assert ) {
-			assert.strictEqual( QUnit.config.module, "urlParams", "module" );
+			assert.strictEqual( QUnit.config.module, "urlParams module", "module" );
+			assert.strictEqual( QUnit.config.filter, "urlParams module", "filter" );
 			assert.strictEqual( QUnit.config.notrycatch, true, "notrycatch" );
 		} );
 
