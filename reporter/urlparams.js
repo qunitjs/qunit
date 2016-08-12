99fa7a63c17ab2d73baadbac1e889a0ac336a076
Richard Gibson <richard.gibson@gmail.com>
2016-08-11 23:33:00 -0400

    fixup! Use a prototype-free object

--- reporter/urlparams.js
+++ reporter/urlparams.js
@@ -1,6 +1,5 @@
 import QUnit from "../src/core";
 import { window } from "../src/globals";
-import { hasOwn } from "../src/core/utilities";
 
 ( function() {
 
@@ -74,7 +73,7 @@ QUnit.begin( function() {
 
 function getUrlParams() {
 	var i, param, name, value;
-	var urlParams = {};
+	var urlParams = Object.create( null );
 	var params = location.search.slice( 1 ).split( "&" );
 	var length = params.length;
 
@@ -86,7 +85,7 @@ function getUrlParams() {
 			// Allow just a key to turn on a flag, e.g., test.html?noglobals
 			value = param.length === 1 ||
 				decodeQueryParam( param.slice( 1 ).join( "=" ) ) ;
-			if ( hasOwn.call( urlParams, name ) ) {
+			if ( name in urlParams ) {
 				urlParams[ name ] = [].concat( urlParams[ name ], value );
 			} else {
 				urlParams[ name ] = value;
