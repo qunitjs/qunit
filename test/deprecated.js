8112a5a70dbe3ed8d1e1ced6d40d6a05a4045fc7
Manoj Kumar <nithmanoj@gmail.com>
2016-09-06 20:22:14 +0000

    Core: Add test in deprecated.js

--- test/deprecated.js
+++ test/deprecated.js
@@ -77,6 +77,12 @@ QUnit.test( "QUnit.stop", function( assert ) {
 	}, /use QUnit\.test\(\) with assert\.async\(\) instead\./ );
 } );
 
+QUnit.test( "QUnit.jsDump", function( assert ) {
+	assert.throws( function() {
+		QUnit.jsDump();
+	}, /Use QUnit\.dump instead of QUnit\.jsDump/ );
+} );
+
 QUnit.test( "QUnit.start inside text context", function( assert ) {
 	assert.throws( function() {
 		QUnit.start();
