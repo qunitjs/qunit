qunitjs/qunit

commit 5333de89dda55691e1634081e6fe97033185f836

HTML Reporter: Collapse details for successive failed tests
Fixes #664
Closes #867
Closes #769

reporter/html.js
@@ -63,6 +63,7 @@ QUnit.init = function() {
 };
 
 var config = QUnit.config,
+	collapseNext = false,
 	hasOwn = Object.prototype.hasOwnProperty,
 	defined = {
 		document: window.document !== undefined,
@@ -746,6 +747,16 @@ QUnit.testDone(function( details ) {
 	}
 
 	if ( bad === 0 ) {
+
+		// Collapse the passing tests
+		addClass( assertList, "qunit-collapsed" );
+	} else if ( bad && config.collapse && !collapseNext ) {
+
+		// Skip collapsing the first failing test
+		collapseNext = true;
+	} else {
+
+		// Collapse remaining tests
 		addClass( assertList, "qunit-collapsed" );
 	}
 

src/core/config.js
@@ -17,6 +17,10 @@ var config = {
 	// by default, modify document.title when suite is done
 	altertitle: true,
 
+	// HTML Reporter: collapse every test except the first failing test
+	// If false, all failing tests will be expanded
+	collapse: true,
+
 	// by default, scroll to top of the page when suite is done
 	scrolltop: true,
 

commit ab86969e4375bd7a4812b32ec184382f686a5dd7

HTML Reporter: Fix expanding failed tests when collapse is false. (#1033)
Fix a regression introduced from 5333de8

reporter/html.js
@@ -816,14 +816,16 @@ QUnit.testDone( function( details ) {
 
 		// Collapse the passing tests
 		addClass( assertList, "qunit-collapsed" );
-	} else if ( bad && config.collapse && !collapseNext ) {
+	} else if ( config.collapse ) {
+		if ( !collapseNext ) {
 
-		// Skip collapsing the first failing test
-		collapseNext = true;
-	} else {
+			// Skip collapsing the first failing test
+			collapseNext = true;
+		} else {
 
-		// Collapse remaining tests
-		addClass( assertList, "qunit-collapsed" );
+			// Collapse remaining tests
+			addClass( assertList, "qunit-collapsed" );
+		}
 	}
 
 	// The testItem.firstChild is the test name