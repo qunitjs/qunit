454af5637afc5c4f2120fb2acf6cafb4361fac63
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2016-11-12 14:00:00 -0600

    Tests: Fixing lint errors caught by eslint-plugin-html

--- test/autostart.js
+++ test/autostart.js
@@ -1,11 +1,9 @@
-/*global times, beginData */
-
 QUnit.start();
 
 QUnit.module( "autostart" );
 
 QUnit.test( "Prove the test run started as expected", function( assert ) {
 	assert.expect( 2 );
-	assert.ok( times.autostartOff <= times.runStarted );
-	assert.strictEqual( beginData.totalTests, 1, "Should have expected 1 test" );
+	assert.ok( window.times.autostartOff <= window.times.runStarted );
+	assert.strictEqual( window.beginData.totalTests, 1, "Should have expected 1 test" );
 } );
