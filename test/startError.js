c223772c0f9408f9bab9f625c7156fe4e27b54b2
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2016-12-04 23:57:19 -0600

    fixup! Using window to assign globals for consistency

--- test/startError.js
+++ test/startError.js
@@ -1,16 +1,14 @@
-/*global autostartStartError, tooManyStartsError */
-
 QUnit.module( "global start unrecoverable errors" );
 
 QUnit.test( "start() throws when QUnit.config.autostart === true", function( assert ) {
 	assert.expect( 1 );
-	assert.equal( autostartStartError.message,
+	assert.equal( window.autostartStartError.message,
 		"Called start() outside of a test context when QUnit.config.autostart was true" );
 } );
 
 QUnit.test( "Throws after calling start() too many times outside of a test context",
 		function( assert ) {
 	assert.expect( 1 );
-	assert.equal( tooManyStartsError.message,
+	assert.equal( window.tooManyStartsError.message,
 		"Called start() outside of a test context too many times" );
 } );
