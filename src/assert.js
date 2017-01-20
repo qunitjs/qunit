88ef0252b732a559fdf14d76c3eade341117f401
Invis1ble <Invis1ble@users.noreply.github.com>
2017-01-21 01:19:29 +0300

    Core: Fix console error in IE9 (#1093)
    Fixes #1092

--- src/assert.js
+++ src/assert.js
@@ -57,8 +57,13 @@ class Assert {
 	// Exports test.push() to the user API
 	// Alias of pushResult.
 	push( result, actual, expected, message, negative ) {
-		console.warn( "assert.push is deprecated and will be removed in QUnit 3.0." +
-			" Please use assert.pushResult instead (http://api.qunitjs.com/pushResult/)." );
+
+		// Supports IE9, not throwing if console is not active.
+		// Ref #1092
+		if ( console ) {
+			console.warn( "assert.push is deprecated and will be removed in QUnit 3.0." +
+				" Please use assert.pushResult instead (http://api.qunitjs.com/pushResult/)." );
+		}
 
 		let currentAssert = this instanceof Assert ? this : config.current.assert;
 		return currentAssert.pushResult( {
