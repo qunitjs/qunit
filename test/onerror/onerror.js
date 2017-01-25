94cc17d9cab3c36429c0987f219fab7bb546f823
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2017-01-24 20:10:40 -0600

    Core: Extract main onError logic into exported function and add tests
    Refs gh-1061

---
+++ test/onerror/onerror.js
@@ -0,0 +1,28 @@
+QUnit.module( "QUnit.onError", function() {
+	QUnit.test( "Should call pushFailure when inside a test", function( assert ) {
+		assert.expect( 3 );
+
+		QUnit.config.current.pushFailure = function( message, source ) {
+			assert.strictEqual( message, "Error message", "Message is correct" );
+			assert.strictEqual( source, "filePath.js:1", "Source is correct" );
+		};
+
+		var result = QUnit.onError( "Error message", "filePath.js", 1 );
+
+		assert.strictEqual( result, false, "onError should allow other error handlers to run" );
+	} );
+
+	QUnit.test( "Shouldn't push failure if ignoreGlobalErrors is enabled", function( assert ) {
+		assert.expect( 1 );
+
+		QUnit.config.current.pushFailure = function() {
+			assert.ok( false, "No error should be pushed" );
+		};
+
+		QUnit.config.current.ignoreGlobalErrors = true;
+
+		var result = QUnit.onError( "Error message", "filePath.js", 1 );
+
+		assert.strictEqual( result, true, "onError should allow other error handlers to run" );
+	} );
+} );
