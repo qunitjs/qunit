bb6ea8a07d7790409292562fb318c1c64137fd70
Trent Willis <twillis@linkedin.com>
2017-02-07 19:35:55 -0800

    Core: Add remaining js-reporter events

---
+++ src/reports/suite.js
@@ -0,0 +1,90 @@
+export default class SuiteReport {
+	constructor( name, parentSuite ) {
+		this.name = name;
+		this.fullName = parentSuite ? parentSuite.fullName.concat( name ) : [];
+
+		this.tests = [];
+		this.childSuites = [];
+
+		if ( parentSuite ) {
+			parentSuite.pushChildSuite( this );
+		}
+	}
+
+	start( recordTime ) {
+		if ( recordTime ) {
+			this._startTime = Date.now();
+		}
+
+		return {
+			name: this.name,
+			fullName: this.fullName.slice(),
+			tests: this.tests.map( test => test.start() ),
+			childSuites: this.childSuites.map( suite => suite.start() ),
+			testCounts: {
+				total: this.getTestCounts().total
+			}
+		};
+	}
+
+	end( recordTime ) {
+		if ( recordTime ) {
+			this._endTime = Date.now();
+		}
+
+		return {
+			name: this.name,
+			fullName: this.fullName.slice(),
+			tests: this.tests.map( test => test.end() ),
+			childSuites: this.childSuites.map( suite => suite.end() ),
+			testCounts: this.getTestCounts(),
+			runtime: this.getRuntime(),
+			status: this.getStatus()
+		};
+	}
+
+	pushChildSuite( suite ) {
+		this.childSuites.push( suite );
+	}
+
+	pushTest( test ) {
+		this.tests.push( test );
+	}
+
+	getRuntime() {
+		return this._endTime - this._startTime;
+	}
+
+	getTestCounts( counts = { passed: 0, failed: 0, skipped: 0, todo: 0, total: 0 } ) {
+		counts = this.tests.reduce( ( counts, test ) => {
+			counts[ test.getStatus() ]++;
+			counts.total++;
+			return counts;
+		}, counts );
+
+		return this.childSuites.reduce( ( counts, suite ) => {
+			return suite.getTestCounts( counts );
+		}, counts );
+	}
+
+	getStatus() {
+		let {
+			total,
+			failed,
+			skipped,
+			todo
+		} = this.getTestCounts();
+
+		if ( failed ) {
+			return "failed";
+		} else {
+			if ( skipped === total ) {
+				return "skipped";
+			} else if ( todo === total ) {
+				return "todo";
+			} else {
+				return "passed";
+			}
+		}
+	}
+}
---
+++ src/reports/test.js
@@ -0,0 +1,76 @@
+import { extend } from "../core/utilities";
+
+export default class TestReport {
+	constructor( name, suite, options ) {
+		this.name = name;
+		this.suiteName = suite.name;
+		this.fullName = suite.fullName.concat( name );
+		this.runtime = 0;
+		this.assertions = [];
+
+		this.skipped = !!options.skip;
+		this.todo = !!options.todo;
+
+		this._startTime = 0;
+		this._endTime = 0;
+
+		suite.pushTest( this );
+	}
+
+	start( recordTime ) {
+		if ( recordTime ) {
+			this._startTime = Date.now();
+		}
+
+		return {
+			name: this.name,
+			suiteName: this.suiteName,
+			fullName: this.fullName.slice()
+		};
+	}
+
+	end( recordTime ) {
+		if ( recordTime ) {
+			this._endTime = Date.now();
+		}
+
+		return extend( this.start(), {
+			runtime: this.getRuntime(),
+			status: this.getStatus(),
+			errors: this.getFailedAssertions(),
+			assertions: this.getAssertions()
+		} );
+	}
+
+	pushAssertion( assertion ) {
+		this.assertions.push( assertion );
+	}
+
+	getRuntime() {
+		return this._endTime - this._startTime;
+	}
+
+	getStatus() {
+		if ( this.skipped ) {
+			return "skipped";
+		}
+
+		let testPassed = this.getFailedAssertions().length > 0 ? this.todo : !this.todo;
+
+		if ( !testPassed ) {
+			return "failed";
+		} else if ( this.todo ) {
+			return "todo";
+		} else {
+			return "passed";
+		}
+	}
+
+	getFailedAssertions() {
+		return this.assertions.filter( assertion => !assertion.passed );
+	}
+
+	getAssertions() {
+		return this.assertions.slice();
+	}
+}
