8ca8fee07cd6c0d9d2165d1da4b66d92707650d2
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2017-02-12 20:18:57 -0600

    Tests: Adding tests for calling context in onerror tests

--- test/reporter-html/window-onerror-preexisting-handler.js
+++ test/reporter-html/window-onerror-preexisting-handler.js
@@ -1,9 +1,14 @@
-/* global onerrorReturnValue */
+/* global onerrorReturnValue, onerrorCallingContext */
 /* exported onerrorReturnValue */
 
-QUnit.module( "window.onerror (with preexisting handler)", function( ) {
+QUnit.module( "window.onerror (with preexisting handler)", function( hooks ) {
+	hooks.beforeEach( function() {
+		onerrorReturnValue = null;
+		onerrorCallingContext = null;
+	} );
+
 	QUnit.test( "Should call QUnit.onError/assert if handler returns false", function( assert ) {
-		assert.expect( 3 );
+		assert.expect( 4 );
 
 		onerrorReturnValue = false;
 
@@ -21,11 +26,12 @@ QUnit.module( "window.onerror (with preexisting handler)", function( ) {
 
 		var result = window.onerror( "An error message", "filename.js", 1 );
 
+		assert.strictEqual( onerrorCallingContext, window, "Handler called with correct context" );
 		assert.strictEqual( result, false, "QUnit.onError should have returned false" );
 	} );
 
 	QUnit.test( "Should call QUnit.onError/assert if handler returns void 0", function( assert ) {
-		assert.expect( 3 );
+		assert.expect( 4 );
 
 		onerrorReturnValue = void 0;
 
@@ -43,11 +49,12 @@ QUnit.module( "window.onerror (with preexisting handler)", function( ) {
 
 		var result = window.onerror( "An error message", "filename.js", 1 );
 
+        assert.strictEqual( onerrorCallingContext, window, "Handler called with correct context" );
 		assert.strictEqual( result, false, "QUnit.onError should have returned false" );
 	} );
 
 	QUnit.test( "Should call QUnit.onError/assert if handler returns truthy", function( assert ) {
-		assert.expect( 3 );
+		assert.expect( 4 );
 
 		onerrorReturnValue = "truthy value";
 
@@ -65,11 +72,12 @@ QUnit.module( "window.onerror (with preexisting handler)", function( ) {
 
 		var result = window.onerror( "An error message", "filename.js", 1 );
 
+        assert.strictEqual( onerrorCallingContext, window, "Handler called with correct context" );
 		assert.strictEqual( result, false, "QUnit.onError should have returned false" );
 	} );
 
 	QUnit.test( "Should not assert if ignoreGlobalFailures config enabled", function( assert ) {
-		assert.expect( 1 );
+		assert.expect( 2 );
 
 		QUnit.config.current.ignoreGlobalErrors = true;
 
@@ -89,11 +97,12 @@ QUnit.module( "window.onerror (with preexisting handler)", function( ) {
 		// Restore assert.pushResult so the next assertions work
 		assert.pushResult = originalPushResult;
 
+        assert.strictEqual( onerrorCallingContext, window, "Handler called with correct context" );
 		assert.strictEqual( result, true, "QUnit.onError should have returned true" );
 	} );
 
 	QUnit.test( "Should not call QUnit.onError if other handler returns true", function( assert ) {
-		assert.expect( 1 );
+		assert.expect( 2 );
 
 		onerrorReturnValue = true;
 
@@ -113,6 +122,7 @@ QUnit.module( "window.onerror (with preexisting handler)", function( ) {
 		// Restore assert.pushResult so the next assertions work
 		assert.pushResult = originalPushResult;
 
+        assert.strictEqual( onerrorCallingContext, window, "Handler called with correct context" );
 		assert.strictEqual( result, true, "Handler should have returned true" );
 	} );
 } );
