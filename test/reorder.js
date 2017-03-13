5c6de6975de8a13ce1725d5fdcf8c955b0fcddf1
Trent Willis <twillis@linkedin.com>
2017-03-13 09:44:45 -0700

    Test: Add basic reorder test

---
+++ test/reorder.js
@@ -0,0 +1,15 @@
+window.sessionStorage.setItem( "qunit-test-Reorder-Second", 1 );
+
+var lastTest = "";
+
+QUnit.module( "Reorder" );
+
+QUnit.test( "First", function( assert ) {
+  assert.strictEqual( lastTest, "Second" );
+  lastTest = "First";
+} );
+
+QUnit.test( "Second", function( assert ) {
+  assert.strictEqual( lastTest, "" );
+  lastTest = "Second";
+} );
