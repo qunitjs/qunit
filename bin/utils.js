184736105d3af6084d5279251c78e90cfa48f44c
Trent Willis <twillis@linkedin.com>
2017-03-16 15:03:03 -0700

    CLI: Introduce QUnit CLI

---
+++ bin/utils.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const walkSync = require( "walk-sync" );
+const existsStat = require( "exists-stat" );
+
+function getFilesFromArgs( args ) {
+	let globs = args.slice();
+
+	// Default to files in the test directory
+	if ( !globs.length ) {
+		globs.push( "test/**/*.js" );
+	}
+
+	// For each of the potential globs, we check if it is a directory path and
+	// update it so that it matches the JS files in that directory.
+	globs = globs.map( glob => {
+		const stat = existsStat( glob );
+
+		if ( stat && stat.isDirectory() ) {
+			return `${glob}/**/*.js`;
+		} else {
+			return glob;
+		}
+	} );
+
+	const files = walkSync( process.cwd(), { globs } );
+
+	if ( !files.length ) {
+		error( "No files were found matching: " + args.join( ", " ) );
+	}
+
+	return files;
+}
+
+function error( message ) {
+	console.error( message );
+	process.exit( 1 );
+}
+
+module.exports = {
+	error,
+	getFilesFromArgs
+};
