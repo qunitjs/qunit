3a9d983e6df8ba166e22b92db2ea4d2966a6cd72
Trent Willis <twillis@linkedin.com>
2017-03-28 13:59:59 -0700

    CLI: Properly support aborting and restarting tests

--- test/cli/fixtures/expected/watch-tap-outputs.js
+++ test/cli/fixtures/expected/watch-tap-outputs.js
@@ -16,6 +16,7 @@ ok 1 foo
 # todo 0
 # fail 0
 File changed: watching/foo.js
+Restarting...
 TAP version 13
 ok 1 bar
 1..1
@@ -33,6 +34,7 @@ ok 1 foo
 # todo 0
 # fail 0
 File added: watching/bar.js
+Restarting...
 TAP version 13
 ok 1 bar
 ok 2 foo
@@ -52,10 +54,30 @@ ok 2 foo
 # todo 0
 # fail 0
 File removed: watching/bar.js
+Restarting...
 TAP version 13
 ok 1 foo
 1..1
 # pass 1
+# skip 0
+# todo 0
+# fail 0
+Stopping QUnit...`,
+
+	"change-file-mid-run": `TAP version 13
+File added: watching/bar.js
+Finishing current test and restarting...
+ok 1 Foo > one
+1..2
+# pass 2
+# skip 0
+# todo 0
+# fail 0
+TAP version 13
+ok 1 Foo > one
+ok 2 Foo > two
+1..2
+# pass 2
 # skip 0
 # todo 0
 # fail 0
