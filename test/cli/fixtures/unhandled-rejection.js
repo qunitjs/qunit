fcae826d9ecdd550c531885e2a52eb33bb28902d
Robert Jackson <rjackson@linkedin.com>
2017-12-18 14:24:18 -0500

    Tests: Update CLI test and test fixture to remove stacktrace ambiguity.
    Unfortunately, we cannot use real errors here because the stack trace
    representation changes across Node versions (and therefore the
    expectations fail).

--- test/cli/fixtures/unhandled-rejection.js
+++ test/cli/fixtures/unhandled-rejection.js
@@ -10,12 +10,25 @@ QUnit.module( "Unhandled Rejections", function() {
 			setTimeout( resolve );
 		} )
 			.then( function() {
-				throw new Error( "Error thrown in non-returned promise!" );
+
+				// throwing a non-Error here because stack trace representation
+				// across Node versions is not stable (they continue to get better)
+				throw {
+					message: "Error thrown in non-returned promise!",
+					stack: `Error: Error thrown in non-returned promise!
+    at /some/path/wherever/unhandled-rejection.js:13:11`
+				};
 			} );
 
 		// prevent test from exiting before unhandled rejection fires
 		setTimeout( done, 10 );
 	} );
 
-	Promise.reject( { message: "outside of a test context" } );
+	// rejecting with a non-Error here because stack trace representation
+	// across Node versions is not stable (they continue to get better)
+	Promise.reject( {
+		message: "outside of a test context",
+		stack: `Error: outside of a test context
+    at Object.<anonymous> (/some/path/wherever/unhandled-rejection.js:20:18)`
+	} );
 } );
