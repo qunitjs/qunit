9264ec2b385d081a1f640345877846977c51bdd1
Trent Willis <twillis@netflix.com>
2018-03-01 11:16:23 -0800

    Test: Refactor startError test to not reload QUnit

--- test/startError.js
+++ test/startError.js
@@ -1,3 +1,16 @@
+try {
+	QUnit.config.autostart = true;
+	QUnit.start();
+} catch ( thrownError ) {
+	window.autostartStartError = thrownError;
+}
+
+try {
+	QUnit.start();
+} catch ( thrownError ) {
+	window.tooManyStartsError = thrownError;
+}
+
 QUnit.module( "global start unrecoverable errors" );
 
 QUnit.test( "start() throws when QUnit.config.autostart === true", function( assert ) {
@@ -6,9 +19,8 @@ QUnit.test( "start() throws when QUnit.config.autostart === true", function( ass
 		"Called start() outside of a test context when QUnit.config.autostart was true" );
 } );
 
-QUnit.test( "Throws after calling start() too many times outside of a test context",
-	function( assert ) {
-		assert.expect( 1 );
-		assert.equal( window.tooManyStartsError.message,
-			"Called start() outside of a test context too many times" );
-	} );
+QUnit.test( "Throws after calling start() too many times outside of a test context", function( assert ) {
+	assert.expect( 1 );
+	assert.equal( window.tooManyStartsError.message,
+		"Called start() outside of a test context too many times" );
+} );
