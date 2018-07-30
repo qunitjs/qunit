9af04486aaf1bf23b602a8f379a7826989a9c89e
Stefan Penner <stefan.penner@gmail.com>
2018-07-30 10:23:27 -0700

    CLI: [fixes #121] chokidar -> sane (#1283)
    I think chokidar is great, but the ergonomics around native dependencies today in node really painful. Although chokidar pre-builds, users switching nodes, or failed pre-build downloads, or behind firewall usages cause a fallback to requiring a working compiler toolchain. Requiring that, very much increases the barrier of entry, and support load for tooling/cli maintainers

--- bin/run.js
+++ bin/run.js
@@ -151,18 +151,18 @@ function watcherEvent( event, args ) {
 }
 
 run.watch = function watch() {
-	const chokidar = require( "chokidar" );
+	const sane = require( "sane" );
 	const args = Array.prototype.slice.call( arguments );
 
-	const watcher = chokidar.watch( "**/*.js", {
-		ignored: IGNORED_GLOBS,
-		ignoreInitial: true
+	const watcher = sane( process.cwd(), {
+		globs: [ "**/*.js" ],
+		ignored: IGNORED_GLOBS
 	} );
 
 	watcher.on( "ready", () => run.apply( null, args ) );
 	watcher.on( "change", watcherEvent( "changed", args ) );
 	watcher.on( "add", watcherEvent( "added", args ) );
-	watcher.on( "unlink", watcherEvent( "removed", args ) );
+	watcher.on( "delete", watcherEvent( "removed", args ) );
 
 	function stop() {
 		console.log( "Stopping QUnit..." );
