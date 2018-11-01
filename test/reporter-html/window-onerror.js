ed8807438670b34763bd422be77525c857e6dabd
Anand Thakker <anandthakker@users.noreply.github.com>
2018-11-01 12:00:42 -0400

    Core: Report full stack trace for uncaught errors if possible (#1324)
    Currently, when QUnit catches an uncaught error in its window.onerror
    handler, it reports it with a source of `fileName + ":" + lineNumber`.
    
    While it's not standard, most modern browsers include an errorObj
    argument to window.onerror, which can be used to get a more complete
    stacktrace
    ([source](https://blog.sentry.io/2016/01/04/client-javascript-reporting-window-onerror)).
    This change modifies the HTML reporter and the `QUnit.onError` method to
    do that.

--- test/reporter-html/window-onerror.js
+++ test/reporter-html/window-onerror.js
@@ -19,6 +19,21 @@ QUnit.module( "window.onerror (no preexisting handler)", function( hooks ) {
 		window.onerror( "An error message", "filename.js", 1 );
 	} );
 
+	QUnit.test( "Should extract stacktrace if it is available", function( assert ) {
+		assert.expect( 1 );
+
+		const errorObj = {
+			stack: "dummy.js:1 top()\ndummy.js:2 middle()\ndummy.js:3 bottom()"
+		};
+
+		QUnit.onError = function( error ) {
+			assert.equal( error.stacktrace, errorObj.stack, "QUnit.onError was called" );
+		};
+
+		window.onerror( "An error message", "filename.js", 1, 1, errorObj );
+	} );
+
+
 	QUnit.test( "Should return QUnit.error return value if it is called", function( assert ) {
 		assert.expect( 1 );
 
