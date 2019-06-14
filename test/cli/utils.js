69533521d88106d83b313368f98a0a7eeb8d7eae
Timo Tijhof <krinklemail@gmail.com>
2019-06-14 01:01:00 +0100

    !fixup rename test

--- test/cli/utils.js
+++ test/cli/utils.js
@@ -1,7 +1,7 @@
 const { getIgnoreList } = require( "../../src/cli/utils" );
 
 QUnit.module( "getIgnoreList", function() {
-	QUnit.test( "reads getIgnoreList", function( assert ) {
+	QUnit.test( "reads .gitignore", function( assert ) {
 		const ignoreList = getIgnoreList( "test/cli/fixtures" );
 		assert.deepEqual( ignoreList, [ "/abcd", "/efgh" ] );
 	} );
