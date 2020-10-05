572439b6d31c7e77f55c2414dc4470652c58e2ec
Timo Tijhof <krinklemail@gmail.com>
2020-10-05 01:51:03 +0100

    Logger: Simplify Logger.warn() internals

--- src/logger.js
+++ src/logger.js
@@ -1,19 +1,9 @@
 import { console } from "./globals";
 
-/**
- * Returns a function that proxies to the given method name on the globals
- * console object. The proxy will also detect if the console doesn't exist and
- * will appropriately no-op. This allows support for IE9, which doesn't have a
- * console if the developer tools are not open.
- */
-function consoleProxy( method ) {
-	return function( ...args ) {
-		if ( console ) {
-			console[ method ]( ...args );
-		}
-	};
-}
+// Detect if the console doesn't exist and no-op in that case.
+// This allows support for IE 9, which doesn't have a console
+// object if the developer tools are not open.
 
 export default {
-	warn: consoleProxy( "warn" )
+	warn: console ? console.warn.bind( console ) : function() {}
 };
