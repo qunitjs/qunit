d927b2b570386a12955815a8ee0b615f5af6427e
Timo Tijhof <krinklemail@gmail.com>
2021-01-11 05:36:30 +0000

    Build: Use qunit/ during development instead of dist/
    Use the same directory structure for the build artefact during
    releases and during development.
    
    Since the release file path is used by published npm and bower
    packages, keep that as-is, and rename the internal development
    target from dist/ to qunit/.
    
    After this, contributors and maintainers will want to delete any
    dist/ directory they might have, to avoid accidental linting or
    publishing.

--- test/cli/require-qunit-test.js
+++ test/cli/require-qunit-test.js
@@ -14,43 +14,30 @@ QUnit.module( "requireQUnit", function( hooks ) {
 		process.chdir( cwd );
 	} );
 
-	QUnit.test( "finds QUnit in the current working directory", function( assert ) {
+	QUnit.test( "finds QUnit package in the current working directory", function( assert ) {
 		const requireQUnit = require( "../../src/cli/require-qunit" );
 		process.chdir( path.join( __dirname, "./fixtures/require-from-cwd" ) );
 
 		assert.propEqual( requireQUnit(), { version: "from-cwd" } );
 	} );
 
-	QUnit.test( "finds globally installed QUnit", function( assert ) {
+	// For development mode invoked locally,
+	// or for global install without local dependency installed.
+	QUnit.test( "finds relative self", function( assert ) {
 		const globalQUnit = {
 			"version": "from-global"
 		};
 		const requireQUnit = proxyquire( "../../src/cli/require-qunit", {
 			"qunit": null,
-			"../../dist/qunit": null,
 			"../../qunit/qunit": globalQUnit
 		} );
 		assert.strictEqual( requireQUnit( resolveStub ), globalQUnit );
 	} );
 
-	QUnit.test( "finds development mode QUnit", function( assert ) {
-		const devQUnit = {
-			"version": "from-dist"
-		};
-		const requireQUnit = proxyquire( "../../src/cli/require-qunit", {
-			"qunit": null,
-			"../../qunit/qunit": null,
-			"../../dist/qunit": devQUnit
-		} );
-
-		assert.strictEqual( requireQUnit( resolveStub ), devQUnit );
-	} );
-
 	QUnit.test( "throws error if none of the modules are found", function( assert ) {
 		const requireQUnit = proxyquire( "../../src/cli/require-qunit", {
 			"qunit": null,
-			"../../qunit/qunit": null,
-			"../../dist/qunit": null
+			"../../qunit/qunit": null
 		} );
 
 		assert.throws( requireQUnit, /Cannot find module/ );
--- test/main/stack.js
+++ test/main/stack.js
@@ -8,7 +8,7 @@
 		assert.true( /(\/|\\)test(\/|\\)main(\/|\\)stack\.js/.test( stack ) );
 
 		stack = QUnit.stack( 2 );
-		assert.true( /(\/|\\)dist(\/|\\)qunit\.js/.test( stack ), "can use offset argument to return a different stacktrace line" );
+		assert.true( /(\/|\\)qunit(\/|\\)qunit\.js/.test( stack ), "can use offset argument to return a different stacktrace line" );
 		assert.false( /\/test\/main\/stack\.js/.test( stack ), "stack with offset argument" );
 	} );
 }() );
