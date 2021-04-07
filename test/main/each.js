603c88b8525368761d360e0c28d8e49761321083
em <ernesto.mudu@gmail.com>
2021-04-07 09:16:39 -0400

    Test: add test with then-able result.

--- test/main/each.js
+++ test/main/each.js
@@ -2,6 +2,14 @@ QUnit.module( "test.each", function() {
 	QUnit.test.each( "test.each", [ [ 1, 2, 3 ], [ 1, 1, 2 ] ], function( assert, a, b, result ) {
 		assert.strictEqual( a + b, result );
 	} );
+	QUnit.test.each( "test.each returning a Promise", [ [ 1, 2, 3 ], [ 1, 1, 2 ] ], function( assert, a, b, expected ) {
+		function sum( a, b ) {
+			return new Promise( function( resolve ) {
+				resolve( a + b );
+			} );
+		}
+		return sum( a, b ).then( function( result ) { assert.strictEqual( result, expected ); } );
+	} );
 	QUnit.test.each( "test.each 1D", [ 1, [], "some" ], function( assert, value ) {
 		assert.true( Boolean( value ) );
 	} );
