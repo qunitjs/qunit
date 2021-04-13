53a04efba3618995b1fc52395d8ad2c2ed55f087
Steve McClure <smcclure15@gmail.com>
2021-04-13 15:43:11 -0400

    Core: Fix missing space in warning message
    Follows f9ce52bf4a54.
    
    Closes https://github.com/qunitjs/qunit/pull/1589.

--- test/cli/main.js
+++ test/cli/main.js
@@ -317,7 +317,7 @@ QUnit.module( "CLI Main", () => {
 		const execution = await execute( command );
 
 		assert.equal( execution.code, 0 );
-		assert.equal( execution.stderr, "The `beforeEach` hook was called inside the wrong module. Instead, use hooks provided by the callback to the containing module.This will become an error in QUnit 3.0.", "The warning shows" );
+		assert.equal( execution.stderr, "The `beforeEach` hook was called inside the wrong module. Instead, use hooks provided by the callback to the containing module. This will become an error in QUnit 3.0.", "The warning shows" );
 		assert.equal( execution.stdout, expectedOutput[ command ] );
 	} );
 } );
