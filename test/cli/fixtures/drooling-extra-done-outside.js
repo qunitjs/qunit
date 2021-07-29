5aae48b44c524bbc5fe821b514da8328cf9fdd73
Timo Tijhof <krinklemail@gmail.com>
2021-07-29 17:14:39 +0100

    Add drooling-extra-done-outside.js

---
+++ test/cli/fixtures/drooling-extra-done-outside.js
@@ -0,0 +1,11 @@
+QUnit.test( "extra done scheduled outside any test", assert => {
+	assert.timeout( 10 );
+	const done = assert.async();
+	assert.true( true );
+
+	// Later, boom!
+	setTimeout( done, 100 );
+
+	// Passing, end of test
+	done();
+} );
