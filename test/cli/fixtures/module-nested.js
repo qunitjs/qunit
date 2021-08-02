1e6b1b2f53c5cc6490f0d59256999562a4c22acf
Timo Tijhof <krinklemail@gmail.com>
2021-08-03 00:00:28 +0100

    Tests: Add module-nested.js test case
    This asserts the undesirable status quo, where the stack is left
    in a bad state if the module closure encounters a global error while
    test suites are still being loaded.
    
    Ref https://github.com/qunitjs/qunit/issues/1478.

---
+++ test/cli/fixtures/module-nested.js
@@ -0,0 +1,37 @@
+try {
+	QUnit.module( "module 1", () => {
+		QUnit.test( "test in module 1", assert => {
+			assert.true( true );
+		} );
+	} );
+} catch ( e ) {
+
+	// Ignore
+}
+
+try {
+	QUnit.module( "module 2", () => {
+
+		// trigger an error in executeNow
+		undefined();
+
+		QUnit.test( "test in module  2", assert => {
+			assert.true( true );
+		} );
+	} );
+} catch ( e ) {
+
+	// Ignore
+}
+
+
+try {
+	QUnit.module( "module 3", () => {
+		QUnit.test( "test in module 3", assert => {
+			assert.true( true );
+		} );
+	} );
+} catch ( e ) {
+
+	// Ignore
+}
