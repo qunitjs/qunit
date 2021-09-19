3239ae5632a96c64b2ce2873663ab7d38ca6c48c
Timo Tijhof <krinklemail@gmail.com>
2021-09-19 01:07:21 +0100

    Tests: Add integration test with grunt-contrib-qunit
    A basic passing test, and an end-to-end "expect failure" test
    to prevent regression https://github.com/qunitjs/qunit/pull/1655.

---
+++ test/integration/grunt-contrib-qunit/Gruntfile.js
@@ -0,0 +1,19 @@
+/* eslint-env node */
+module.exports = function( grunt ) {
+	grunt.loadNpmTasks( "grunt-contrib-qunit" );
+
+	grunt.initConfig( {
+		qunit: {
+			options: {
+				puppeteer: {
+					args: ( process.env.CHROMIUM_FLAGS || ( process.env.CI ? "--no-sandbox" : "" ) )
+						.split( " " )
+				}
+			},
+			pass: [ "pass-*.html" ],
+			"fail-no-tests": "fail-no-tests.html"
+		}
+	} );
+
+	grunt.registerTask( "default", [ "qunit:pass" ] );
+};
