919fadbd88d9c7fdc7d324050ea750e4665cf825
Timo Tijhof <krinkle@fastmail.com>
2022-04-19 00:05:47 +0100

    Test: Replace mozjs ad-hoc reporter with built-in TAP

--- test/mozjs.js
+++ test/mozjs.js
@@ -1,36 +1,13 @@
-/* global loadRelativeToScript, print */
+/* global loadRelativeToScript */
 
 loadRelativeToScript('../qunit/qunit.js');
 
-QUnit.on('runStart', () => {
-  print('Running tests...');
-});
-QUnit.on('testEnd', (testEnd) => {
-  if (testEnd.status === 'todo') {
-    return;
-  }
-  testEnd.errors.forEach((assertion) => {
-    print(`\ntest: ${testEnd.name}\n` +
-      `module: ${testEnd.suiteName}\n` +
-      `message: ${assertion.message}\n${assertion.stack || ''}`);
-  });
-});
+QUnit.reporters.tap.init(QUnit);
 QUnit.on('runEnd', (suiteEnd) => {
-  const stats = suiteEnd.testCounts;
   if (suiteEnd.status === 'failed') {
-    print(`${stats.total} tests in ${suiteEnd.runtime}ms` +
-      `, ${stats.passed} passed` +
-      `, ${stats.failed} failed` +
-      `, ${stats.skipped} skipped` +
-      `, ${stats.todo} todo` +
-      '.'
-    );
-
     // There is no built-in function for sending a non-zero exit code,
     // so throw an uncaught error to make this happen.
     throw new Error('Test run has failures.');
-  } else {
-    print(`${stats.total} tests in ${suiteEnd.runtime}ms, all passed.`);
   }
 });
 
