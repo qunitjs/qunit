ca1cd5118f21498aaa6613914ded80c06632025a
Timo Tijhof <krinkle@fastmail.com>
2022-04-18 01:19:36 +0100

    Test: Add integration test with karma-qunit
    This avoids regressing any APIs that karma-qunit uses to detect
    or report failure details, prior to merging or releasing any
    changes.

---
+++ test/integration/karma-qunit/pass-basic.js
@@ -0,0 +1,9 @@
+QUnit.test('example 1', function (assert) {
+  assert.true(true, 'x');
+});
+QUnit.test('example 2', function (assert) {
+  assert.true(true, 'y');
+});
+QUnit.test('example 3', function (assert) {
+  assert.true(true, 'z');
+});
