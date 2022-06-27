684f6d0fb03c3b7c913d3d8ff2b147866be31cf0
Timo Tijhof <krinkle@fastmail.com>
2022-06-26 21:13:15 -0700

    Test: Update grunt-contrib-qunit to 6.2.0

--- test/integration/grunt-contrib-qunit/Gruntfile.js
+++ test/integration/grunt-contrib-qunit/Gruntfile.js
@@ -4,12 +4,6 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     qunit: {
-      options: {
-        puppeteer: {
-          args: (process.env.CHROMIUM_FLAGS || (process.env.CI ? '--no-sandbox' : ''))
-            .split(' ')
-        }
-      },
       pass: ['pass-*.html'],
       'fail-assert': 'fail-assert.html',
       'fail-no-tests': 'fail-no-tests.html',
