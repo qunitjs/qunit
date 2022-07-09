9cf3e6cb55a2250b751e9c62ed9149a52d83c997
Izel Nakri <contact@izelnakri.com>
2022-07-09 02:07:30 +0200

    Core: Faster globals diffing by doing less slow mutations
    Refactor `diff()` to do less slow mutations.
    Make it use array filter internally.
    
    Closes https://github.com/qunitjs/qunit/pull/1697.

--- src/core/utilities.js
+++ src/core/utilities.js
@@ -43,18 +43,7 @@ export const performance = {
 
 // Returns a new Array with the elements that are in a but not in b
 export function diff (a, b) {
-  const result = a.slice();
-
-  for (let i = 0; i < result.length; i++) {
-    for (let j = 0; j < b.length; j++) {
-      if (result[i] === b[j]) {
-        result.splice(i, 1);
-        i--;
-        break;
-      }
-    }
-  }
-  return result;
+  return a.filter((a) => b.indexOf(a) === -1);
 }
 
 /**
