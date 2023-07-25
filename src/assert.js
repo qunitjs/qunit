c971fa63b227b0276ebc50eb722ae35e7490e070
Zixin Yin <zixinyin@mathworks.com>
2023-07-25 15:45:10 -0400

    Core: add type checking for assert.async

--- src/assert.js
+++ src/assert.js
@@ -68,11 +68,16 @@ class Assert {
 
   // Create a new async pause and return a new function that can release the pause.
   async (count) {
-    const requiredCalls = count === undefined ? 1 : count;
+    if (count === undefined) {
+      count = 1;
+    } else if (typeof count !== 'number') {
+      throw new TypeError('async takes number as an input');
+    }
+    const requiredCalls = count;
     return this.test.internalStop(requiredCalls);
   }
 
-  // Exports test.push() to the user API
+  // goExports test.push() to the user API
   // Alias of pushResult.
   push (result, actual, expected, message, negative) {
     Logger.warn('assert.push is deprecated and will be removed in QUnit 3.0.' +
