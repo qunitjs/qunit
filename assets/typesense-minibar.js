7939ab0ae0cf9489e8b9d875d9a7b28f6cc16fc8
Krinkle <Krinkle@users.noreply.github.com>
2023-10-15 01:37:32 +0000

    Build commit 552ad59ab299ec87a5fab0edc8c9f6ec1b9587e3

--- assets/typesense-minibar.js
+++ assets/typesense-minibar.js
@@ -1,9 +1,23 @@
-/*! https://github.com/jquery/typesense-minibar 1.0.2 */
+/*! https://github.com/jquery/typesense-minibar 1.1.1 */
 globalThis.tsminibar = function tsminibar (form) {
-  const { origin, key, collection } = form.dataset;
+  const { origin, collection } = form.dataset;
   const group = !!form.dataset.group;
   const cache = new Map();
-  const state = { query: '', hits: [], cursor: -1, open: false };
+  const state = { query: '', cursor: -1, open: false, hits: [] };
+  const searchParams = new URLSearchParams({
+    per_page: '5',
+    query_by: 'hierarchy.lvl0,hierarchy.lvl1,hierarchy.lvl2,hierarchy.lvl3,hierarchy.lvl4,hierarchy.lvl5,content',
+    include_fields: 'hierarchy.lvl0,hierarchy.lvl1,hierarchy.lvl2,hierarchy.lvl3,hierarchy.lvl4,hierarchy.lvl5,content,url_without_anchor,url,id',
+    highlight_full_fields: 'hierarchy.lvl0,hierarchy.lvl1,hierarchy.lvl2,hierarchy.lvl3,hierarchy.lvl4,hierarchy.lvl5,content',
+    group_by: 'url_without_anchor',
+    group_limit: '1',
+    sort_by: 'item_priority:desc',
+    snippet_threshold: '8',
+    highlight_affix_num_tokens: '12',
+    'x-typesense-api-key': form.dataset.key,
+    ...Object.fromEntries(new URLSearchParams(form.dataset.searchParams))
+  });
+  const noResults = form.dataset.noResults || "No results for '{}'.";
 
   const input = form.querySelector('input[type=search]');
   const listbox = document.createElement('div');
@@ -12,11 +26,11 @@ globalThis.tsminibar = function tsminibar (form) {
   input.after(listbox);
 
   let preconnect = null;
-  input.addEventListener('focus', () => {
+  input.addEventListener('focus', function () {
     if (!preconnect) {
       preconnect = document.createElement('link');
       preconnect.rel = 'preconnect';
-      preconnect.crossOrigin = 'anonymous'; // for fetch mode:cors,credentials:omit
+      preconnect.crossOrigin = 'anonymous'; // match fetch cors,credentials:omit
       preconnect.href = origin;
       document.head.append(preconnect);
     }
@@ -25,19 +39,12 @@ globalThis.tsminibar = function tsminibar (form) {
       render();
     }
   });
-  input.addEventListener('click', () => {
-    if (!state.open && state.hits.length) {
-      state.open = true;
-      render();
-    }
-  });
-  input.addEventListener('input', async () => {
+  input.addEventListener('input', async function () {
     const query = state.query = input.value;
     if (!query) {
       state.hits = []; // don't leak old hits on focus
       state.cursor = -1;
-      close();
-      return;
+      return close();
     }
     const hits = await search(query);
     if (state.query === query) { // ignore non-current query
@@ -47,8 +54,14 @@ globalThis.tsminibar = function tsminibar (form) {
       render();
     }
   });
-  input.addEventListener('keydown', (e) => {
-    if (!(e.altKey || e.ctrlKey || e.metaKey || e.shiftKey)) {
+  input.addEventListener('click', function () {
+    if (!state.open && state.hits.length) {
+      state.open = true;
+      render();
+    }
+  });
+  input.addEventListener('keydown', function (e) {
+    if (!e.altKey && !e.ctrlKey && !e.metaKey && !e.shiftKey) {
       if (e.code === 'ArrowDown') moveCursor(1);
       if (e.code === 'ArrowUp') moveCursor(-1);
       if (e.code === 'Escape') close();
@@ -58,11 +71,15 @@ globalThis.tsminibar = function tsminibar (form) {
       }
     }
   });
-  form.addEventListener('submit', (e) => {
+  form.addEventListener('submit', function (e) {
     e.preventDefault(); // disable fallback
   });
   form.insertAdjacentHTML('beforeend', '<svg viewBox="0 0 12 12" width="20" height="20" aria-hidden="true" class="tsmb-icon-close" style="display: none;"><path d="M9 3L3 9M3 3L9 9"/></svg>');
-  form.querySelector('.tsmb-icon-close').addEventListener('click', close);
+  form.querySelector('.tsmb-icon-close').addEventListener('click', function () {
+    input.value = '';
+    input.focus();
+    close();
+  });
   connect();
 
   function close () {
@@ -98,30 +115,19 @@ globalThis.tsminibar = function tsminibar (form) {
   }
 
   async function search (query) {
+    let lvl0;
     let hits = cache.get(query);
     if (hits) {
       cache.delete(query);
       cache.set(query, hits); // LRU
       return hits;
     }
+    searchParams.set('q', query);
     const resp = await fetch(
-      `${origin}/collections/${collection}/documents/search?` + new URLSearchParams({
-        q: query,
-        per_page: '5',
-        query_by: 'hierarchy.lvl0,hierarchy.lvl1,hierarchy.lvl2,hierarchy.lvl3,hierarchy.lvl4,hierarchy.lvl5,content',
-        include_fields: 'hierarchy.lvl0,hierarchy.lvl1,hierarchy.lvl2,hierarchy.lvl3,hierarchy.lvl4,hierarchy.lvl5,content,url_without_anchor,url,id',
-        highlight_full_fields: 'hierarchy.lvl0,hierarchy.lvl1,hierarchy.lvl2,hierarchy.lvl3,hierarchy.lvl4,hierarchy.lvl5,content',
-        group_by: 'url_without_anchor',
-        group_limit: '1',
-        sort_by: 'item_priority:desc',
-        snippet_threshold: '8',
-        highlight_affix_num_tokens: '12',
-        'x-typesense-api-key': key,
-      }),
+      `${origin}/collections/${collection}/documents/search?` + searchParams,
       { mode: 'cors', credentials: 'omit', method: 'GET' }
     );
     const data = await resp.json();
-    let lvl0;
     hits = data?.grouped_hits?.map(ghit => {
       const hit = ghit.hits[0];
       return {
@@ -146,7 +152,7 @@ globalThis.tsminibar = function tsminibar (form) {
     listbox.hidden = !state.open;
     form.classList.toggle('tsmb-form--open', state.open);
     if (state.open) {
-      listbox.innerHTML = (state.hits.map((hit, i) => `<div role="option"${i === state.cursor ? ' aria-selected="true"' : ''}>${hit.lvl0 ? `<div class="tsmb-suggestion_group">${hit.lvl0}</div>` : ''}<a href="${hit.url}" tabindex="-1"><div class="tsmb-suggestion_title">${hit.title}</div><div class="tsmb-suggestion_content">${hit.content}</div></a></div>`).join('') || `<div class="tsmb-empty">No results for '${escape(state.query)}'.</div>`) + (form.dataset.foot ? '<a href="https://typesense.org" class="tsmb-foot" title="Search by Typesense"></a>' : '');
+      listbox.innerHTML = (state.hits.map((hit, i) => `<div role="option"${i === state.cursor ? ' aria-selected="true"' : ''}>${hit.lvl0 ? `<div class="tsmb-suggestion_group">${hit.lvl0}</div>` : ''}<a href="${hit.url}" tabindex="-1"><div class="tsmb-suggestion_title">${hit.title}</div><div class="tsmb-suggestion_content">${hit.content}</div></a></div>`).join('') || `<div class="tsmb-empty">${noResults.replace('{}', escape(state.query))}</div>`) + (form.dataset.foot ? '<a href="https://typesense.org" class="tsmb-foot" title="Search by Typesense"></a>' : '');
     }
   }
 
