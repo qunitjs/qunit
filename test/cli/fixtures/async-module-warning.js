14a6a1fc7131e2c5095c031221fb26152e1a9349
Raymond Cohen <rcohen@squareup.com>
2024-06-09 09:51:25 -0400

    Dont catch error for async module warning tap test

--- test/cli/fixtures/async-module-warning.js
+++ test/cli/fixtures/async-module-warning.js
@@ -1,26 +1,8 @@
-var errorFromAsyncModule;
-try {
-  // eslint-disable-next-line qunit/no-async-module-callbacks
-  QUnit.module('module with async callback', async function () {
-    await Promise.resolve(1);
+// eslint-disable-next-line qunit/no-async-module-callbacks
+QUnit.module('module with async callback', async function () {
+  await Promise.resolve(1);
 
-    QUnit.test('has a test', function (assert) {
-      assert.true(true);
-    });
+  QUnit.test('has a test', function (assert) {
+    assert.true(true);
   });
-} catch (e) {
-  errorFromAsyncModule = e;
-}
-
-QUnit.test(
-  'Correct error thrown from async module function',
-  function (assert) {
-    assert.true(errorFromAsyncModule instanceof Error);
-    assert.strictEqual(
-      errorFromAsyncModule.message,
-      'Async module callbacks are not supported. ' +
-        'Instead, use hooks for async behavior.',
-      'Error has correct message'
-    );
-  }
-);
+});
