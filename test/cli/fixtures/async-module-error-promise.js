76ad95aaa2854665c996ad8769b0815dbaf1dec1
Raymond Cohen <rcohen@squareup.com>
2024-06-09 10:08:14 -0400

    tap

--- test/cli/fixtures/async-module-error-promise.js
+++ test/cli/fixtures/async-module-error-promise.js
@@ -1,24 +1,6 @@
-var errorFromModule;
-
-try {
-  QUnit.module('module manually returning a promise', function () {
-    QUnit.test('has a test', function (assert) {
-      assert.true(true);
-    });
-    return Promise.resolve(1);
+QUnit.module('module manually returning a promise', function () {
+  QUnit.test('has a test', function (assert) {
+    assert.true(true);
   });
-} catch (e) {
-  errorFromModule = e;
-}
-
-QUnit.test(
-  'Correct error thrown from module function that returns a promise',
-  function (assert) {
-    assert.true(errorFromModule instanceof Error);
-    assert.strictEqual(
-      errorFromModule.message,
-      'Returning a promise from a module callback is not supported. Instead, use hooks for async behavior.',
-      'Error has correct message'
-    );
-  }
-);
+  return Promise.resolve(1);
+});
