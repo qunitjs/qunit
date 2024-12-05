qunitjs/qunit

commit 47e3b996388623ef6b16aa8ed2a8685e0ca4c872

HTML Reporter: QUnit 3.0 theme structure and layout
Notable design changes:

* Edge to edge design.
  Remove body margin (browser default), remove rounded corners.

* Progress bar.
  The qunit-banner was previously blank until it turns green/red.
  Test execution is now animated with a blue progress bar.

* Adopt system-ui fonts, remove any text shadows.

* Sticky header reduced to toolbar.
  Re-implemented using `position: sticky`, to create a normal
  page-level scrollbar instead of a scrollable area. The rest of the
  page naturally flows under it. This fixes various layout quirks,
  with custom elements no longer squished or pushed outside the viewport.
  This is similar to how things rendered prior to QUnit 2.14.

  The sticky part now excludes the page title and user agent,
  making more optimal use of vertical space.

* Move "Abort" button to the left.

  This previously floated in a somewhat-random position on the right.
  The toolbar now occupies the same height during the "running" and
  "completed" state, avoiding a visible layout shift.

* Remove 2px offset hack from checkboxes to make them vertically aligned.

Other changes:

* `#qunit-userAgent` is now DIV rather than H2,
  placed above #qunit-testrunner-toolbar instead of below it.
* `#qunit-toolbar` now parents `#qunit-banner`, `#qunit-testresult`,
  and `#qunit-filteredTest`.
* `#qunit-header` now parents `H1` and `#qunit-userAgent`.
* The `#qunit-modulefilter-actions` buttons are now ordered in a more
  natural way in the DOM. Previously, the order was reversed, with
  the default theme using float: right to reverse them.
  To achieve right-aligned buttons like before, one could set
  `text-align: right` on `.qunit-modulefilter-actions`.
* The `.qunit-skipped-label` and `.qunit-todo-label` elements, which
  are palced before TODO/SKIP test titles, now include a space between
  the label and the test title. This means the word "skipped" and the

test/main/HtmlReporter.js
@@ -107,7 +107,7 @@ QUnit.test('testresult-display [begin]', function (assert) {
 
   var testresult = element.querySelector('#qunit-testresult');
   assert.equal(testresult.className, 'result', 'testresult class');
-  assert.equal(testresult.textContent, 'Running...\u00A0Abort', 'testresult text');
+  assert.equal(testresult.textContent, 'AbortRunning...\u00A0', 'testresult text');
 
   var display = element.querySelector('#qunit-testresult-display');
   assert.equal(display.className, '', 'display class');
@@ -129,7 +129,7 @@ QUnit.test('testresult-display [testStart]', function (assert) {
 
   var display = element.querySelector('#qunit-testresult-display');
   assert.equal(display.className, 'running', 'display class');
-  assert.equal(display.textContent, '1 / 4 tests completed.Running: B', 'display text');
+  assert.equal(display.textContent, 'Running test 1 of 4: B', 'display text');
 
   var testOutput = element.querySelector('#qunit-test-output-00A');
   assert.equal(

src/qunit.css
@@ -7,81 +7,66 @@
  * https://jquery.org/license
  */
 
-/** Font Family and Sizes */
+/**
+ * Resets
+ */
 
-#qunit-tests, #qunit-header, #qunit-banner, #qunit-testrunner-toolbar, #qunit-filteredTest, #qunit-userAgent, #qunit-testresult {
-	font-family: "Helvetica Neue Light", "HelveticaNeue-Light", "Helvetica Neue", Calibri, Helvetica, Arial, sans-serif;
-}
-
-#qunit-testrunner-toolbar, #qunit-filteredTest, #qunit-userAgent, #qunit-testresult, #qunit-tests li { font-size: small; }
-#qunit-tests { font-size: smaller; }
-
-
-/** Resets */
-
-#qunit-tests, #qunit-header, #qunit-banner, #qunit-filteredTest, #qunit-userAgent, #qunit-testresult, #qunit-modulefilter {
+body {
 	margin: 0;
 	padding: 0;
+}
+
+[id=qunit] * {
+	margin: 0;
+	padding: 0;
+}
+
+#qunit {
+	font-family: system-ui, sans-serif;
+	font-size: 13px;
+	line-height: 1.2;
+	background: #FFF;
+	color: #333;
 }
 
 /* Style our buttons in a simple way, uninfluenced by the styles
    the tested app might load. Don't affect buttons in #qunit-fixture!
    https://github.com/qunitjs/qunit/pull/1395
    https://github.com/qunitjs/qunit/issues/1437 */
-#qunit-testrunner-toolbar button,
-#qunit-testresult button {
-	all: unset; /* best effort, modern browsers only */
-	font: inherit;
-	color: initial;
-	border: initial;
-	background-color: buttonface;
-	padding: 0 4px;
+[id=qunit] button {
+	all: revert; /* best effort, modern browsers only */
+  cursor: pointer;
+}
+[id=qunit] button:disabled {
+  cursor: default;
 }
 
-
-/** Fixed headers with scrollable tests */
-
-@supports (display: flex) or (display: -webkit-box) {
-	@media (min-height: 500px) {
-		#qunit {
-			position: fixed;
-			left: 0;
-			right: 0;
-			top: 0;
-			bottom: 0;
-			padding: 8px;
-			display: -webkit-box;
-			display: flex;
-			flex-direction: column;
-		}
-
-		#qunit-tests {
-			overflow: scroll;
-		}
-
-		#qunit-banner {
-			flex: 5px 0 0;
-		}
-	}
-}
-
-
-/** Header (excluding toolbar) */
+/**
+ * Header
+ */
 
 #qunit-header {
-	padding: 0.5em 0 0.5em 1em;
+	padding: 13px;
 
 	color: #C2CCD1;
 	background-color: #0D3349;
 
-	font-size: 1.5em;
-	line-height: 1em;
-	font-weight: 400;
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: space-between;
+	align-items: flex-end;
+	gap: 13px;
+}
 
-	border-radius: 5px 5px 0 0;
+#qunit-header h1 {
+	flex-grow: 1;
+	font-size: 24px;
+	line-height: 1;
+	font-weight: 400;
 }
 
 #qunit-header a {
+	display: block;
 	text-decoration: none;
 	color: inherit;
 }
@@ -92,135 +77,140 @@
 }
 
 #qunit-banner {
+	--qunit-progress: 0%;
+
 	height: 5px;
+	background-image: linear-gradient(#0769AD, #0769AD);
+	background-repeat: no-repeat;
+	background-size: var(--qunit-progress);
+	transition: background-size 100ms ease-out;
+}
+#qunit-banner.qunit-pass {
+	background-image: none;
+	background-color: #C6E746;
+}
+#qunit-banner.qunit-fail {
+	background-image: none;
+	background-color: #EE5757;
+}
+
+#qunit-toolbar {
+	background-color: #EEE;
+	border-bottom: 1px solid #FFF;
+}
+
+#qunit-testresult {
+	clear: both;
+	border-top: 1px solid #FFF;
+}
+
+#qunit-toolbar-urlconfig,
+#qunit-toolbar-filters,
+#qunit-testresult,
+#qunit-filteredTest {
+	padding: 7px 13px;
 }
 
 #qunit-filteredTest {
-	padding: 0.5em 1em 0.5em 1em;
-	color: #366097;
-	background-color: #F4FF77;
+	border-top: 1px solid #FFF;
+	background-color: #fbdbfb;
 }
 
-#qunit-userAgent {
-	padding: 0.5em 1em 0.5em 1em;
-	color: #FFF;
-	background-color: #2B81AF;
-	text-shadow: rgba(0, 0, 0, 0.5) 2px 2px 1px;
+/** Fixed toolbar, with scrollable test results */
+@supports (position: sticky) {
+  @media (min-height: 400px) {
+    #qunit-toolbar {
+      position: sticky;
+      top: 0px;
+      box-shadow: rgba(13, 51, 73, 0.3) 0px 5px 10px -5px;
+    }
+  }
 }
 
+/**
+ * Toolbar elements
+ */
 
-/** Toolbar */
+#qunit-toolbar label {
+	display: inline-block;
+	margin-right: 6px;
+	line-height: 2.1;
+}
+
+#qunit-toolbar form {
+	display: inline-block;
+}
+
+#qunit-toolbar input[type=checkbox],
+#qunit-toolbar input[type=radio] {
+	margin: 3px;
+}
+
+#qunit-toolbar input[type=text] {
+	box-sizing: border-box;
+	height: 30px;
+	padding: 0 6px;
+}
+
+/**
+ * Toolbar layout
+ */
 
 #qunit-testrunner-toolbar {
-	padding: 0.5em 1em 0.5em 1em;
-	color: #5E740B;
-	background-color: #EEE;
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: space-between;
 }
-
-#qunit-testrunner-toolbar .clearfix {
-	height: 0;
-	clear: both;
-}
-
-#qunit-testrunner-toolbar label {
-	display: inline-block;
-}
-
-#qunit-testrunner-toolbar input[type=checkbox],
-#qunit-testrunner-toolbar input[type=radio] {
-	margin: 3px;
-	vertical-align: -2px;
-}
-
-#qunit-testrunner-toolbar input[type=text] {
-	box-sizing: border-box;
-	height: 1.6em;
-}
-
-#qunit-testrunner-toolbar button,
-#qunit-testresult button {
-  border-radius: .25em;
-  border: 1px solid #AAA;
-  background-color: #F8F8F8;
-  color: #222;
-  line-height: 1.6;
-  cursor: pointer;
-}
-#qunit-testrunner-toolbar button:hover,
-#qunit-testresult button:hover {
-	border-color: #AAA;
-	background-color: #FFF;
-	color: #444;
-}
-#qunit-testrunner-toolbar button:active,
-#qunit-testresult button:active {
-	border-color: #777;
-	background-color: #CCC;
-	color: #000;
-}
-#qunit-testrunner-toolbar button:focus,
-#qunit-testresult button:focus {
-	border-color: #2F68DA;
-	/* emulate 2px border without a layout shift */
-	box-shadow: inset 0 0 0 1px #2F68DA
-}
-#qunit-testrunner-toolbar button:disabled,
-#qunit-testresult button:disabled {
-	border-color: #CCC;
-	background-color: #CCC;
-	color: #FFF;
-  cursor: default;
-}
-
 #qunit-toolbar-filters {
-	float: right;
-	/* aligning right avoids overflows and inefficient use of space
-	   around the dropdown menu on narrow viewports */
-	text-align: right;
-}
+	flex-grow: 1;
+	justify-content: flex-end;
 
-.qunit-url-config,
-.qunit-filter,
+	display: flex;
+	flex-flow: row wrap;
+	gap: 13px;
+}
 #qunit-modulefilter {
-	display: inline-block;
-	line-height: 2.1em;
-	text-align: left;
+	flex-grow: 1;
+	max-width: 450px;
 }
-
-.qunit-filter,
-#qunit-modulefilter {
-	position: relative;
-	margin-left: 1em;
+#qunit-modulefilter label {
+	margin-right: 0;
+	display: flex;
+	flex-flow: row wrap;
 }
-
-.qunit-url-config label {
-	margin-right: 0.5em;
-}
-
 #qunit-modulefilter-search {
-	box-sizing: border-box;
-	min-width: 400px;
-	min-width: min(400px, 80vw);
+	width: 100%;
 }
+
+@media (max-width: 1000px) {
+  #qunit-toolbar-filters {
+		justify-content: flex-start;
+  }
+}
+
+/**
+ * Module selector
+ */
 
 #qunit-modulefilter-search-container {
+	/* grow within `#qunit-modulefilter label` */
+	flex-grow: 1;
 	position: relative;
 }
 #qunit-modulefilter-search-container:after {
 	position: absolute;
-	right: 0.3em;
+	right: 6px;
 	bottom: 0;
-	line-height: 100%;
-	content: "\25bc";
-	color: black;
+	height: 30px; /* align with input[type=text] */
+	width: 16px;
+	content: "";
+  background: url('data:image/svg+xml,<svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path clip-rule="evenodd" fill-rule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"></path></svg>') center no-repeat;
 }
 
 #qunit-modulefilter-dropdown {
 	/* align with #qunit-modulefilter-search */
 	box-sizing: border-box;
-	min-width: 400px;
-	min-width: min(400px, 80vw);
-	max-width: 80vw;
+	width: 100%;
 	position: absolute;
 	right: 0;
 	top: 100%;
@@ -236,37 +226,25 @@
 	color: #0D3349;
 	background-color: #F5F5F5;
 	z-index: 99;
+	max-height: min(80vh, 700px);
+  overflow-y: auto;
 }
 
 #qunit-modulefilter-actions {
 	display: block;
 	overflow: auto;
 	/* align with #qunit-modulefilter-dropdown-list */
-	font: smaller/1.5em sans-serif;
-}
-@media (min-width: 350px) {
-	#qunit-modulefilter-actions {
-		position: absolute;
-		right: 0;
-	}
+	line-height: 1.5;
 }
 
-#qunit-modulefilter-dropdown #qunit-modulefilter-actions > * {
-	box-sizing: border-box;
+#qunit-modulefilter-actions button {
 	max-height: 2.8em;
-	display: block;
-	padding: 0.4em;
-}
-
-#qunit-modulefilter-dropdown #qunit-modulefilter-actions > button {
-	float: right;
 	margin: 0.25em;
 }
 
 #qunit-modulefilter-dropdown-list {
 	margin: 0;
 	padding: 0;
-	font: smaller/1.5em sans-serif;
 }
 
 #qunit-modulefilter-dropdown-list li {
@@ -289,8 +267,29 @@
 	color: #444;
 }
 
+/** Test Result */
 
-/** Tests: Pass/Fail */
+#qunit-testresult {
+	/* left #qunit-testresult-display, right #qunit-testresult-controls */
+	display: flex;
+	flex-flow: row wrap;
+	align-items: center;
+	gap: 13px;
+}
+#qunit-testresult a {
+	color: #2F68DA;
+}
+#qunit-testresult .module-name {
+	font-weight: 700;
+}
+#qunit-testresult-controls {
+	max-width: 10%;
+}
+#qunit-testresult-controls:empty {
+	display: none;
+}
+
+/** Test output */
 
 #qunit-tests {
 	list-style-position: inside;
@@ -342,8 +341,6 @@
 	padding: 0.5em;
 
 	background-color: #FFF;
-
-	border-radius: 5px;
 }
 
 .qunit-source {
@@ -387,11 +384,17 @@
 	text-decoration: none;
 }
 
-/*** Test Counts */
+/** Test output: Counts */
 
-#qunit-tests b.counts                       { color: #0D3349; }
-#qunit-tests b.passed                       { color: #5E740B; }
-#qunit-tests b.failed                       { color: #710909; }
+#qunit-tests .counts {
+	color: #0D3349;
+}
+#qunit-tests .passed {
+	color: #5E740B;
+}
+#qunit-tests .failed {
+	color: #710909;
+}
 
 #qunit-tests li li {
 	padding: 5px;
@@ -400,8 +403,7 @@
 	list-style-position: inside;
 }
 
-/*** Passing Styles */
-
+/** Test output: Passing */
 
 #qunit-tests .pass {
 	color: #2F68DA;
@@ -421,9 +423,8 @@
 #qunit-tests .pass .test-actual,
 #qunit-tests .pass .test-expected           { color: #999; }
 
-#qunit-banner.qunit-pass                    { background-color: #C6E746; }
 
-/*** Failing Styles */
+/** Test output: Failing */
 
 #qunit-tests .fail {
 	color: #000;
@@ -437,19 +438,15 @@
 	white-space: pre;
 }
 
-#qunit-tests > li:last-child {
-	border-radius: 0 0 5px 5px;
-}
-
 #qunit-tests .fail .test-actual             { color: #EE5757; }
 #qunit-tests .fail .test-expected           { color: #008000; }
 
-#qunit-banner.qunit-fail                    { background-color: #EE5757; }
 
+/** Test output: Aborted */
 
-/*** Aborted tests */
 #qunit-tests .aborted { color: #000; background-color: orange; }
-/*** Skipped tests */
+
+/** Test output: Skipped */
 
 #qunit-tests .skipped {
 	background-color: #EBECE9;
@@ -463,40 +460,11 @@
 	color: #366097;
 	line-height: 1.8em;
 	padding: 0 0.5em;
-	margin: -0.4em 0.4em -0.4em 0;
+	margin: -0.4em 0 -0.4em 0;
 }
 
 #qunit-tests .qunit-todo-label {
 	background-color: #EEE;
-}
-
-/** Result */
-
-#qunit-testresult {
-	color: #366097;
-	background-color: #E2F0F7;
-
-	border-bottom: 1px solid #FFF;
-}
-#qunit-testresult a {
-	color: #2F68DA;
-}
-#qunit-testresult .clearfix {
-	height: 0;
-	clear: both;
-}
-#qunit-testresult .module-name {
-	font-weight: 700;
-}
-#qunit-testresult-display {
-	padding: 0.5em 1em 0.5em 1em;
-	width: 85%;
-	float:left;
-}
-#qunit-testresult-controls {
-	padding: 0.5em 1em 0.5em 1em;
-  width: 10%;
-	float:left;
 }
 
 /** Fixture */

commit 73c03cf27745e179396a6d7c9af011a20d3b9082

HTML Reporter: Fix broken "Rerun without a depth limit" link (really)
Follows-up 91db92dbc5, which recently fixed another part of the
`maxDepth` feature, but left the HTML still invalid.

Follows-up 47e3b99638, which broke the HTML by mistakenly swapping
one of the double quotes for a single quote, thus leaving the anchor
element unclosed.

demos/qunit-config-maxDepth.html
@@ -1,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>qunit-config-maxDepth</title>
+  <link rel="stylesheet" href="../src/core/qunit.css">
+</head>
+<body>
+  <div id="qunit"></div>
+  <script src="../qunit/qunit.js"></script>
+  <script>
+  QUnit.test('example', function (assert) {
+    // Assume default of QUnit.config.maxDepth of 5.
+    assert.deepEqual(
+      {
+        one: {
+          two: {
+            three: {
+              four: {
+                five: {
+                  six: true
+                }
+              }
+            }
+          }
+        }
+      },
+      {
+        one: {
+          two: {
+            three: {
+              four: {
+                five: {
+                  six: false
+                }
+              }
+            }
+          }
+        }
+      }
+    );
+  });
+  </script>
+</body>
+</html>

src/core/qunit.css
@@ -313,11 +313,13 @@ body {
 	cursor: pointer;
 }
 
-.qunit-test a {
+.qunit-test > a {
 	padding: 0.5em;
+	user-select: none;
+}
+.qunit-test a {
 	color: inherit;
 	text-decoration: underline;
-	user-select: none;
 }
 .qunit-test a:hover,
 .qunit-test a:focus {

src/core/reporters/HtmlReporter.js
@@ -900,14 +900,17 @@ export default class HtmlReporter {
           message += '<tr class="test-diff"><th>Diff: </th><td><pre>'
             + diffHtml + '</pre></td></tr>';
         }
-      } else if (expected.indexOf('[object Array]') !== -1
-        || expected.indexOf('[object Object]') !== -1) {
+      } else if (
+        expected.indexOf('[object Array]') !== -1
+        || expected.indexOf('[object Object]') !== -1
+      ) {
+        // To test this interatively, use /demos/qunit-config-maxDepth.html
         message += '<tr class="test-message"><th>Message: </th><td>'
-          + 'Diff suppressed as the depth of object is more than current max depth ('
-          + this.config.maxDepth + ').<p>Hint: Use <code>QUnit.dump.maxDepth</code> to '
-          + ' run with a higher max depth or <a href="'
-          + escapeText(this.makeUrl({ maxDepth: 0 })) + "'>"
-          + 'Rerun</a> without max depth.</p></td></tr>';
+          + 'Diff suppressed because the object is more than ' + this.config.maxDepth
+          + ' levels deep.<p>Hint: Use <code>QUnit.config.maxDepth</code> to '
+          + 'set a higher limit, or <a href="'
+          + escapeText(this.makeUrl({ maxDepth: 0 })) + '">Rerun without a depth limit</a>'
+          + '.</p><br/></td></tr>';
       } else {
         message += '<tr class="test-message"><th>Message: </th><td>'
           + 'Diff suppressed as the expected and actual results have an equivalent'

test/main/HtmlReporter.js
@@ -235,6 +235,68 @@ QUnit.test('test-output [trace]', function (assert) {
   );
 });
 
+// For an interative test, refer to /demos/qunit-config-maxDepth.html
+QUnit.test('test-output [maxDepth exceeded]', function (assert) {
+  var element = document.createElement('div');
+  new QUnit.reporters.html(this.MockQUnit, {
+    element: element,
+    config: {
+      maxDepth: 2,
+      urlConfig: []
+    }
+  });
+  QUnit.dump.maxDepth = 2;
+
+  this.MockQUnit.emit('runStart', { testCounts: { total: 1 } });
+  this.MockQUnit.emit('begin', { modules: [] });
+  this.MockQUnit.emit('testStart', { testId: '00A', name: 'A' });
+  this.MockQUnit.emit('log', {
+    testId: '00A',
+    message: 'boo',
+    result: false,
+    actual: {
+      one: {
+        two: {
+          three: 1
+        }
+      }
+    },
+    expected: {
+      one: {
+        two: {
+          three: 2
+        }
+      }
+    },
+    runtime: 1
+  });
+  this.MockQUnit.emit('testDone', {
+    testId: '00A',
+    name: 'A',
+    total: 1,
+    passed: 1,
+    failed: 0,
+    runtime: 2
+  });
+
+  var testOutput = element.querySelector('#qunit-test-output-00A');
+  assert.strictEqual(
+    testOutput.textContent,
+    'A (1)' + 'Rerun' + '2 ms' + 'boo' + '@ 1 ms'
+      + 'Expected: {\n'
+      + '  "one": {\n'
+      + '    "two": [object Object]\n'
+      + '  }\n'
+      + '}'
+      + 'Message: Diff suppressed because the object is more than 2 levels deep.'
+      + 'Hint: Use QUnit.config.maxDepth to set a higher limit, or Rerun without a depth limit.',
+    'test output'
+  );
+
+  // Restore default
+  QUnit.dump.maxDepth = 5;
+});
+
 QUnit.test('onError [early]', function (assert) {
   var element = document.createElement('div');
   new QUnit.reporters.html(this.MockQUnit, {